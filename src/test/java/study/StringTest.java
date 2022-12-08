0387590533a83bc28af5b87fc2d51e089ba5b418
pyun0825 <pyun0825@naver.com>
2022-12-08 23:37:20 +0900

    test: implement JUnit exercise tests

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -2,12 +2,35 @@ package study;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
-    void replace() {
-        String actual = "abc".replace("b", "d");
-        assertThat(actual).isEqualTo("adc");
+    void splitOneAndTwo() {
+        String str = "1,2";
+        String[] resStr = str.split(",");
+        assertThat(resStr).contains("1", "2");
+    }
+
+    @Test
+    void splitOne() {
+        String str = "1";
+        String[] resStr = str.split(",");
+        assertThat(resStr).containsExactly("1");
+    }
+
+    @Test
+    void removeParenthesis() {
+        String str = "(1,2)";
+        String resStr = str.substring(1, 4);
+        assertThat(resStr).isEqualTo("1,2");
+    }
+
+    @Test
+    void strIdxOutOfBound() {
+        String str = "abc";
+        assertThatExceptionOfType(IndexOutOfBoundsException.class).isThrownBy(() -> {
+            str.charAt(3);
+        }).withMessageMatching("Index \\d+ out of bounds for length \\d+");
     }
 }
