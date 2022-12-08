f4d0af2fd59dbb7a73a52a895fda26f2b52b704a
kakao <kakao@kakaoui-MacBookPro.local>
2022-12-09 00:48:19 +0900

    refactor(): 매직넘버 제거
    매직넘버 제거하기 위해 상수 정의 및 관련 변수명&메소드명 수정

--- src/main/java/precourse/NumberBaseball.java
+++ src/main/java/precourse/NumberBaseball.java
@@ -5,6 +5,18 @@ import java.util.HashSet;
 import java.util.Scanner;
 
 public class NumberBaseball {
+    private static final int TARGET_LENGTH = 3;
+    private static final int BOUND = 9;
+    private static final String ZERO = "0";
+    private static final int STRIKE = 0;
+    private static final int BALL = 1;
+    private static final int THREE_STRIKE = 3;
+    private static final boolean CONTINUE = true;
+    private static final boolean QUIT = false;
+    private static final boolean SUCCESS = true;
+    private static final boolean FAIL = false;
+    private static final int RESTART = 1;
+
     private String target;
 
     public String getTarget() {
@@ -18,8 +30,8 @@ public class NumberBaseball {
         String number = "";
         Random rand = new Random();
         rand.setSeed(System.currentTimeMillis());
-        while (number.length() < 3) {
-            int numInt = rand.nextInt(9);
+        while (number.length() < TARGET_LENGTH) {
+            int numInt = rand.nextInt(BOUND);
             String numStr = Integer.toString(numInt);
             number += this.checkValidNumber(numStr, number);
         }
@@ -27,7 +39,7 @@ public class NumberBaseball {
     }
 
     public String checkValidNumber(String num, String tmp) {
-        if (num.equals("0") || tmp.contains(num)) {
+        if (num.equals(ZERO) || tmp.contains(num)) {
             return "";
         }
         return num;
@@ -35,40 +47,40 @@ public class NumberBaseball {
 
     public boolean examine(String ans) {
         int[] result = {0, 0}; // 0 : strike, 1 :  ball
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < TARGET_LENGTH; i++) {
             this.examineUnit(i, ans.substring(i, i+1), result);
         }
         printResult(result);
-        if (result[0] == 3) {
-            return true;
+        if (result[STRIKE] == THREE_STRIKE) {
+            return SUCCESS;
         }
-        return false;
+        return FAIL;
     }
 
-    public void examineUnit(int i, String num, int[] result) {
-        if (this.target.indexOf(num) == i) {
-            result[0]++;
+    public void examineUnit(int numIndex, String num, int[] result) {
+        if (this.target.indexOf(num) == numIndex) {
+            result[STRIKE]++;
             return;
         }
         if (this.target.contains(num)) {
-            result[1]++;
+            result[BALL]++;
         }
     }
 
     public void printResult(int[] result) {
-        if (result[0] == 0 && result[1] == 0) {
+        if (result[STRIKE] == 0 && result[BALL] == 0) {
             System.out.println("낫싱");
             return;
         }
-        if (result[1] == 0) {
-            System.out.println(String.format("%d 스트라이크", result[0]));
+        if (result[BALL] == 0) {
+            System.out.println(String.format("%d 스트라이크", result[STRIKE]));
             return;
         }
-        if (result[0] == 0) {
-            System.out.println(String.format("%d 볼", result[1]));
+        if (result[STRIKE] == 0) {
+            System.out.println(String.format("%d 볼", result[BALL]));
             return;
         }
-        System.out.println(String.format("%d 스트라이크 %d 볼", result[0], result[1]));
+        System.out.println(String.format("%d 스트라이크 %d 볼", result[STRIKE], result[BALL]));
     }
 
     public String input() {
@@ -88,31 +100,31 @@ public class NumberBaseball {
         for (char c : input.toCharArray()) {
             set.add(c);
         }
-        return !(input.length() != 3 || input.contains("0") || set.size() != 3);
+        return !(input.length() != TARGET_LENGTH || input.contains(ZERO) || set.size() != TARGET_LENGTH);
     }
 
-    public boolean continueGame() {
+    public boolean restartGame() {
         Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt() == 1;
+        return scanner.nextInt() == RESTART;
     }
 
     public void newGame() {
         this.setTarget();
-        boolean isOver = false;
-        while (!isOver) {
+        boolean over = QUIT;
+        while (!over) {
             String input = this.input();
-            isOver = this.examine(input);
+            over = this.examine(input);
         }
     }
 
     public static void main(String[] args) {
         NumberBaseball nb = new NumberBaseball();
-        boolean doing = true;
-        while (doing) {
+        boolean play = CONTINUE;
+        while (play) {
             nb.newGame();
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            doing = nb.continueGame();
+            play = nb.restartGame();
         }
         System.out.println("게임을 종료합니다.");
     }
