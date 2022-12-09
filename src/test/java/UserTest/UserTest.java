68bee9dd4a40e5b0096ebabf3c5af111f0297fb7
kakao <kakao@bakergus-pc.local>
2022-12-09 12:04:36 +0900

    test: 테스트코드 수정
    UserTest의 테스트케이스 일부 제거

--- src/test/java/UserTest/UserTest.java
+++ src/test/java/UserTest/UserTest.java
@@ -9,14 +9,7 @@ public class UserTest {
     void setNumbersTest() {
         User user = new User();
 
-        user.setNumbers("312");
-        assertThat(user.getNumbers().size()).isEqualTo(3);
-        assertThat(user.getNumbers()).containsExactly(3,1,2);
-        user.setNumbers("4312");
-        assertThat(user.getNumbers().size()).isEqualTo(4);
-        assertThat(user.getNumbers()).containsExactly(4,3,1,2);
-        user.setNumbers("02");
-        assertThat(user.getNumbers().size()).isEqualTo(2);
-        assertThat(user.getNumbers()).containsExactly(0,2);
+        user.setNumbers("043012");
+        assertThat(user.getNumbers()).containsExactly(0,4,3,0,1,2);
     }
 }
