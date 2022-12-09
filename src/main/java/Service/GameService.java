ea3b22620ceebcfd639dd0f46043170761b4a501
kimtaehyun98 <kimtaehyun98@naver.com>
2022-12-09 21:58:37 +0900

    feat(Game): 입력된 숫자에 대한 결과 계산

--- src/main/java/Service/GameService.java
+++ src/main/java/Service/GameService.java
@@ -46,5 +46,50 @@ public class GameService {
     public boolean checkUserInputNumbers(){
         return errorService.checkUserInputNumber();
     }
+
+    public int isStrike(char num, char cmp){
+        return num == cmp ? 1 : 0;
+    }
+
+    public int isBall(String num, char cmp){
+        return num.contains(Character.toString(cmp)) == true ? 1 : 0;
+    }
+
+    public boolean printResult(int strike, int ball, int nothing){
+        if(ball > 0) System.out.println(ball + "볼 ");
+        if(strike > 0) System.out.println(strike + "스트라이크");
+        if(nothing == 3) System.out.println("낫싱");
+
+        return strike == 3 ? true : false; // 게임이 끝났으면 true, 아니면 false
+    }
+
+    public boolean compareLogic(String numbers, String userNumber){
+        int ballCnt = 0, strikeCnt = 0, nothingCnt = 0;
+        for(int i=0; i<3; i++) {
+            char num = numbers.charAt(i);
+            char cmp = userNumber.charAt(i);
+            strikeCnt += isStrike(num, cmp);
+            ballCnt += isBall(numbers, cmp);
+        }
+        ballCnt -= strikeCnt; // ball에는 Strike도 측정되기 때문에
+        nothingCnt = 3 - (ballCnt + strikeCnt);
+        return printResult(strikeCnt, ballCnt, nothingCnt);
+    }
+
+    public String changeArrayToString(ArrayList<Integer> arr){
+        int ret = 0;
+        for(int i : arr) {
+            ret += (ret * 10) + i;
+        }
+        return Integer.toString(ret);
+    }
+
+    public void compareWithAnswer(){
+        ArrayList<Integer> numbersArr = gameRepository.getNumbers();
+        String numbers = changeArrayToString(numbersArr);
+        String userNumber = gameRepository.getUserNumber();
+
+        boolean endFlag = compareLogic(numbers, userNumber);
+    }
 }
 
