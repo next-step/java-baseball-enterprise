d6cfe841283e32937d4dde2ee7fa56f93d439b6b
kyne.k <dd2307@snu.ac.kr>
2022-12-11 17:44:16 +0900

    feat: GameController 구현
    - 생성자를 통해 InputView, OutputView 초기화(final)
    - play() 메소드 구현
      - 게임 초기화 후 실행
      - 각 게임 종료 후 재실행 여부에 대한 입력을 받도록 구현
    - playGame() 메소드 구현 : 각 게임 내에서의 실행 기능
      - 사용자의 입력을 받아 StrikeBallCount 결과 생성
      - 결과 출력
      - 게임 종료 여부를 확인하여 종료할 때까지 실행

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,9 +1,43 @@
 package baseball.controller;
 
+import baseball.BaseballNumbersGenerator;
+import baseball.domain.BaseballGame;
+import baseball.domain.StrikeBallCount;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 
 public class GameController {
-    InputView inputView = new InputView();
-    OutputView outputView = new OutputView();
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final BaseballNumbersGenerator baseballNumbersGenerator;
+    private BaseballGame baseballGame;
+    private StrikeBallCount strikeBallCount;
+
+    public GameController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        baseballNumbersGenerator = new BaseballNumbersGenerator();
+    }
+
+    public void play() {
+        do {
+            initializeGame();
+            playGame();
+        } while (inputView.readInputRestart());
+    }
+
+    private void initializeGame() {
+        baseballGame = new BaseballGame(baseballNumbersGenerator.generate());
+    }
+
+    private void playGame() {
+        do {
+            strikeBallCount = baseballGame.getResult(inputView.readInputUserAnswer());
+            printResult();
+        } while (!strikeBallCount.getIsComplete());
+    }
+
+    private void printResult() {
+        outputView.printResult(strikeBallCount);
+    }
 }
