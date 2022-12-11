d73dccae50de9a324a26b78b6665030602e19853
kyne.k <dd2307@snu.ac.kr>
2022-12-11 18:00:39 +0900

    feat: BaseballGame.java 구현
    - 생성자로 정답을 입력받고, 이를 활용한 심판 생성
    - getResult() 메소드 구현
      - userNumbers를 인자로 받아 판정 결과 반환

--- src/main/java/baseball/domain/BaseballGame.java
+++ src/main/java/baseball/domain/BaseballGame.java
@@ -1,4 +1,11 @@
 package baseball.domain;
 
 public class BaseballGame {
+    private final Judge judge;
+    public BaseballGame(BaseballNumbers baseballNumbers) {
+        judge = new Judge(baseballNumbers);
+    }
+    public StrikeBallCount getResult(BaseballNumbers userNumbers) {
+        return judge.judge(userNumbers);
+    }
 }
