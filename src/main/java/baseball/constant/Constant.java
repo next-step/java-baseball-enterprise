a387c28a718359e91cfd5499305083d841442357
kyne.k <dd2307@snu.ac.kr>
2022-12-11 18:28:29 +0900

    feat: Constant.java 생성 및 구현
    - 게임 전반에서 쓰이는 상수를 관리하는 enum class
    - getValue() 메소드 생성

---
+++ src/main/java/baseball/constant/Constant.java
@@ -0,0 +1,15 @@
+package baseball.constant;
+
+public enum Constant {
+    LENGTH(3), MIN_RANGE(1), MAX_RANGE(9);
+
+    private final int value;
+
+    Constant(int number) {
+        this.value = number;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
