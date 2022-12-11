ca86aa39e14245f29faf8083634cef6f4758564a
kyne.k <dd2307@snu.ac.kr>
2022-12-11 18:37:28 +0900

    feat: StrikeBallCount.java 구현
    - reset() 메소드 : 스트라이크, 볼 카운트 모두 0으로 초기화
    - strikeCount, ballCount에 대한 getter 구현
    - addStrikeCount(), addBallCount() : strikeCount, ballCount 각각을 1 늘리는 함수
    - getIsComplete() : 길이와 strikeCount가 일치하는지 확인 -> 게임이 종료되는지 확인

--- src/main/java/baseball/domain/StrikeBallCount.java
+++ src/main/java/baseball/domain/StrikeBallCount.java
@@ -1,4 +1,36 @@
 package baseball.domain;
 
+import baseball.constant.Constant;
+
 public class StrikeBallCount {
+    private int strikeCount;
+    private int ballCount;
+    private final int length;
+
+    public StrikeBallCount(){
+        length = Constant.LENGTH.getValue();
+    }
+
+    public void resetCounts(){
+        strikeCount = 0;
+        ballCount = 0;
+    }
+    public void addStrikeCount(){
+        strikeCount++;
+    }
+    public void addBallCount(){
+        ballCount++;
+    }
+
+    public int getStrikeCount(){
+        return strikeCount;
+    }
+
+    public int getBallCount(){
+        return ballCount;
+    }
+
+    public boolean getIsComplete() {
+        return strikeCount == length;
+    }
 }
