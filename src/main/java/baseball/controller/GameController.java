00ef69c51cb54845703991f7192039ae2a778bb0
kyne.k <dd2307@snu.ac.kr>
2022-12-11 17:13:30 +0900

    chore: 최초 클래스 설계
    - 패키지 및 클래스 구조 생성

---
+++ src/main/java/baseball/controller/GameController.java
@@ -0,0 +1,9 @@
+package baseball.controller;
+
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+public class GameController {
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+}
