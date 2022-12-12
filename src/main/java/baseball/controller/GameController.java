98c5dcd5928a7ecc86d817679d83509aa56915a8
kyne.k <dd2307@snu.ac.kr>
2022-12-12 22:41:11 +0900

    refactor: 정답 시 메시지 출력 구현
    - 기존 누락된 "3개의 숫자를 모두 맞히셨습니다! 게임 종료" 문자열 출력 추가

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,12 +1,15 @@
 package baseball.controller;
 
 import baseball.BaseballNumbersGenerator;
+import baseball.constant.Constant;
 import baseball.domain.BaseballGame;
 import baseball.domain.StrikeBallCount;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 
 public class GameController {
+    private static final String END_FORMAT = "%d개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String END_MESSAGE = String.format(END_FORMAT, Constant.LENGTH.getValue());
     private final InputView inputView;
     private final OutputView outputView;
     private final BaseballNumbersGenerator baseballNumbersGenerator;
@@ -23,6 +26,7 @@ public class GameController {
         do {
             initializeGame();
             playGame();
+            System.out.println(END_MESSAGE);
         } while (inputView.readInputRestart());
     }
 
