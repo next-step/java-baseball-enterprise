ce8d77aa3d9597435b3a8d04ebe3d13c209468d2
GrasshopperBears <starday456@naver.com>
2022-12-12 16:42:36 +0900

    test: SetTest setup - numbers set 정의 - 각 테스트 실행 전 기본 set 설정

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,21 @@
+package study;
+
+import org.junit.jupiter.api.*;
+
+import java.util.Set;
+import java.util.HashSet;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+}
