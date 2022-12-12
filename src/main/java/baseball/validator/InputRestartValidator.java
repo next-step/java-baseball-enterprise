895c3202ff8e1c0a88d94a3efb85a2be3f1307f6
kyne.k <dd2307@snu.ac.kr>
2022-12-12 21:59:42 +0900

    feat: InputRestartValidator.java 구현
    - validate 함수 구현
    - 잘못된 입력의 경우 예외 처리

--- src/main/java/baseball/validator/InputRestartValidator.java
+++ src/main/java/baseball/validator/InputRestartValidator.java
@@ -1,4 +1,17 @@
 package baseball.validator;
 
-public class InputRestartValidator {
+public class InputRestartValidator extends Validator{
+    private static final String RESTART = "1";
+    private static final String QUIT = "2";
+
+    public static Boolean validate(String input) {
+        String trimmedInput = removeSpace(input);
+        if (trimmedInput.equals(RESTART)){
+            return true;
+        }
+        if (trimmedInput.equals(QUIT)){
+            return false;
+        }
+        throw new IllegalArgumentException();
+    }
 }
