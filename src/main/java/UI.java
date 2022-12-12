2f2a322ed6ac06fc970c496e989f2781a66057f7
Subin Min <tnqls5417@naver.com>
2022-12-12 11:52:27 +0900

    feat: add user output method to UI
    details:
    - output method receives int[] {strike count, ball count}
    - print calculation result to user

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -13,4 +13,17 @@ public class UI {
         }
         return input.trim();
     }
+
+    public void output(int[] result) {
+        if (result[0]!=0) {
+            System.out.print(result[0] + " 스트라이크 ");
+        }
+        if (result[1]!=0) {
+            System.out.print(result[1] + " 볼 ");
+        }
+        if (result[0]==0 & result[1]==0) {
+            System.out.print("낫싱");
+        }
+        System.out.println();
+    }
 }
