579cb30713ec84c152b274d43b07fd70a2085944
herbi.sf8 <herbi.sf8@kakaocorp.com>
2022-12-13 00:24:14 +0900

    feat($userInput): validate user input
    new feature:
    - 사용자 input 받기
    - input 검증
        - 입력이 3자리인가?
        - 입력이 숫자(1~9)로 이루어져있는가?
        - 입력에 중복된 숫자가 없는가?

--- src/main/java/study/BaseballGame.java
+++ src/main/java/study/BaseballGame.java
@@ -2,19 +2,25 @@ package study;
 
 import java.util.HashSet;
 import java.util.Random;
+import java.util.Scanner;
 import java.util.Set;
 
 public class BaseballGame {
 
     private final String answer;
+    private final int DIGITS;
+
     public BaseballGame() {
+        DIGITS = 3;
+        answer = generateAnswer();
+    }
+    private String generateAnswer() {
         Set<Integer> hashSet = new HashSet<Integer>();
         String numbers = "";
-        int DIGITS = 3;
         for (int i =0; i<DIGITS; i++) {
             numbers += generateNumber(hashSet);
         }
-        answer = numbers;
+        return numbers;
     }
 
     private String generateNumber(Set<Integer> hashSet) {
@@ -26,5 +32,52 @@ public class BaseballGame {
             hashSet.add(randomNum);
         }
         return Integer.toString(randomNum);
+    }
+
+    public void getUserInput() {
+        System.out.println("숫자를 입력해주세요: ");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        boolean result = validateUserInput(userInput);
+        scanner.close();
+    }
+
+    private boolean validateUserInput(String userInput) {
+        if (!validate_length(userInput)) {
+            throw new IllegalArgumentException("입력이 3자리가 아닙니다.");
+        }
+        else if (!validate_consistOfNumbers(userInput)) {
+            throw new IllegalArgumentException("입력이 숫자로 구성돼있지 않습니다.");
+        }
+        else if (!validate_duplicateNumbers(userInput)) {
+            throw new IllegalArgumentException("중복된 숫자가 입력됐습니다.");
+        }
+        return true;
+    }
+
+    private boolean validate_duplicateNumbers(String userInput) {
+        Set<Character> hashSet = new HashSet<Character>();
+        for(int i =0; i<userInput.length(); i++) {
+            hashSet.add(userInput.charAt(i));
+        }
+        return hashSet.size() == DIGITS;
+    }
+
+
+    private boolean validate_length(String UserInput) {
+        return UserInput.length() == DIGITS;
+    }
+    private boolean validate_consistOfNumbers(String userInput) {
+
+        int flag = 1;
+        for(int i = 0; i < userInput.length(); i++) {
+            flag *= validate_number(userInput.charAt(i));
+        }
+        return flag == 1;
+    }
+    private int validate_number(char c) {
+        if (c >= '1' && c <= '9')
+            return 1;
+        return 0;
     }
 }
--- src/test/java/study/BaseballGameTest.java
+++ src/test/java/study/BaseballGameTest.java
@@ -2,12 +2,19 @@ package study;
 
 import org.assertj.core.api.Assertions;
 import org.assertj.core.error.ShouldBeFalse;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.lang.reflect.Field;
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class BaseballGameTest {
@@ -22,6 +29,7 @@ public class BaseballGameTest {
         answer = String.valueOf(answerField.get(bg));
         System.out.println("answer = " + answer);
     }
+
     @RepeatedTest(100)
     @DisplayName("정답생성: 숫자가 3자리로 이루어져있는가??")
     void numberGenerateTest1() {
@@ -56,5 +64,61 @@ public class BaseballGameTest {
     @DisplayName("정답생성: 0이 포함돼있지 않는가?")
     void numberGenerateTest4() {
         Assertions.assertThat(answer.contains("0")).isFalse();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"01", "dsada", "dw", "fwqf", "\n", " ", "d"})
+    @DisplayName("유저입력: 입력이 3자리가 아닐 때")
+    void userInputTest1(String input) {
+        InputStream in;
+
+        in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        Assertions.assertThatThrownBy(() -> {
+            bg.getUserInput();
+        }).isInstanceOf(IllegalArgumentException.class).hasMessage("입력이 3자리가 아닙니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"12a", "aaa", "13a", "a21"})
+    @DisplayName("유저입력: 입력이 숫자로 구성돼있지 않을 때")
+    void userInputTest2(String input) {
+        InputStream in;
+
+        in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        Assertions.assertThatThrownBy(() -> {
+            bg.getUserInput();
+        }).isInstanceOf(IllegalArgumentException.class).hasMessage("입력이 숫자로 구성돼있지 않습니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"121", "333", "133", "225"})
+    @DisplayName("유저입력: 중복된 숫자가 입력됐을 때")
+    void userInputTest3(String input) {
+        InputStream in;
+
+        in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        Assertions.assertThatThrownBy(() -> {
+            bg.getUserInput();
+        }).isInstanceOf(IllegalArgumentException.class).hasMessage("중복된 숫자가 입력됐습니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"123", "532", "198", "236"})
+    @DisplayName("유저입력: 정상적인 숫자가 입력됐을 때")
+    void userInputTest4(String input) {
+        InputStream in;
+
+        in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        Assertions.assertThatNoException().isThrownBy(() -> {
+            bg.getUserInput();
+        });
     }
 }
