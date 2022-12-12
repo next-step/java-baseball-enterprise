2a399bec37ca89150b821ae7ef6c42f630781676
kyne.k <dd2307@snu.ac.kr>
2022-12-12 22:39:58 +0900

    refactor: 사용자 입력 검증 과정에서의 예외 메시지 구현

--- src/main/java/baseball/validator/InputRestartValidator.java
+++ src/main/java/baseball/validator/InputRestartValidator.java
@@ -3,6 +3,7 @@ package baseball.validator;
 public class InputRestartValidator extends Validator{
     private static final String RESTART = "1";
     private static final String QUIT = "2";
+    private static final String INVALID_RESTART_INPUT_ERROR_MESSAGE = "[ERROR] 입력을 확인해주세요.";
 
     public static Boolean validate(String input) {
         String trimmedInput = removeSpace(input);
@@ -12,6 +13,6 @@ public class InputRestartValidator extends Validator{
         if (trimmedInput.equals(QUIT)){
             return false;
         }
-        throw new IllegalArgumentException();
+        throw new IllegalArgumentException(INVALID_RESTART_INPUT_ERROR_MESSAGE);
     }
 }
