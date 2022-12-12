44857ae17832eb31f9b02541a9edbc28ef05c541
kyne.k <dd2307@snu.ac.kr>
2022-12-12 13:25:01 +0900

    feat: BaseballNumbersGenerator.java 구현
    - java.util.Random 클래스 활용
    - baseballNumbers.java의 add method 활용

--- src/main/java/baseball/BaseballNumbersGenerator.java
+++ src/main/java/baseball/BaseballNumbersGenerator.java
@@ -1,4 +1,24 @@
 package baseball;
 
+import baseball.constant.Constant;
+import baseball.domain.BaseballNumbers;
+
+import java.util.Random;
+
 public class BaseballNumbersGenerator {
+    Random random;
+    private final int MIN = Constant.MIN_RANGE.getValue();
+    private final int MAX = Constant.MAX_RANGE.getValue();
+    public BaseballNumbersGenerator(){
+        random = new Random();
+    }
+    public BaseballNumbers generate() {
+        BaseballNumbers baseballNumbers = new BaseballNumbers();
+        Boolean isFull = false;
+        while(!isFull) {
+            int nextNumber = MIN + random.nextInt(MAX-MIN);
+            isFull = baseballNumbers.add(nextNumber);
+        }
+        return baseballNumbers;
+    }
 }
