d623397e77e89671e00fac6c58f2961143ae8d54
kyne.k <dd2307@snu.ac.kr>
2022-12-12 13:00:01 +0900

    feat: BaseballNumbers.java 구현
    - List<Integer>의 형태로 숫자 저장
    - contains, add, isFull, getIndex 메소드를 구현

--- src/main/java/baseball/domain/BaseballNumbers.java
+++ src/main/java/baseball/domain/BaseballNumbers.java
@@ -1,4 +1,34 @@
 package baseball.domain;
 
+import baseball.constant.Constant;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BaseballNumbers {
+    private List<Integer> numbers;
+    private final int length = Constant.LENGTH.getValue();
+
+    public BaseballNumbers(){
+        List<Integer> numbers = new ArrayList<>();
+    }
+
+    public boolean contains(int number){
+        return numbers.contains(number);
+    }
+
+    public boolean add(int number){
+        if (!contains(number)){
+            numbers.add(number);
+        }
+        return isFull();
+    }
+
+    public boolean isFull(){
+        return numbers.size() == length;
+    }
+
+    public int getIndex(int number){
+        return numbers.indexOf(number);
+    }
 }
