5adcf1c85448fd24ab7b7d3605029825b3410f83
GrasshopperBears <starday456@naver.com>
2022-12-12 16:12:56 +0900

    test: implement requirements 1 - split했을 경우 잘 분리되는지 확인 - regex가 없는 경우 string이 그대로 반환되는지 확인

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,23 @@
 package study;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
-    void replace() {
-        String actual = "abc".replace("b", "d");
-        assertThat(actual).isEqualTo("adc");
+    @DisplayName("Split multiple parts")
+    void splitMultiple() {
+        String source = "1,2";
+        String[] actual = source.split(",");
+        assertThat(actual).contains("1", "2");
+    }
+
+    @Test
+    @DisplayName("Split single part")
+    void splitSingle() {
+        String source = "1";
+        String[] actual = source.split(",");
+        assertThat(actual).containsExactly("1");
     }
 }
