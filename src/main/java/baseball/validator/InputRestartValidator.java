12332ec000b37af36d73f6caaee0356a9ae40399
kyne.k <dd2307@snu.ac.kr>
2022-12-12 23:02:52 +0900

    refactor: Command enum class 추가
    - 재시작 여부 command를 enum class로 관리
    - 이를 반영하여 InputRestartValidator, InputView 수정

--- src/main/java/baseball/validator/InputRestartValidator.java
+++ src/main/java/baseball/validator/InputRestartValidator.java
@@ -1,16 +1,18 @@
 package baseball.validator;
 
+import baseball.constant.Command;
+
 public class InputRestartValidator extends Validator {
-    private static final String RESTART = "1";
-    private static final String QUIT = "2";
+    private static final String restart = Command.RESTART.getValue();
+    private static final String quit = Command.QUIT.getValue();
     private static final String INVALID_RESTART_INPUT_ERROR_MESSAGE = "[ERROR] 입력을 확인해주세요.";
 
     public static Boolean validate(String input) {
         String trimmedInput = removeSpace(input);
-        if (trimmedInput.equals(RESTART)) {
+        if (trimmedInput.equals(restart)) {
             return true;
         }
-        if (trimmedInput.equals(QUIT)) {
+        if (trimmedInput.equals(quit)) {
             return false;
         }
         throw new IllegalArgumentException(INVALID_RESTART_INPUT_ERROR_MESSAGE);
--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,5 +1,6 @@
 package baseball.view;
 
+import baseball.constant.Command;
 import baseball.domain.BaseballNumbers;
 import baseball.validator.InputRestartValidator;
 import baseball.validator.InputUserAnswerValidator;
@@ -7,8 +8,10 @@ import baseball.validator.InputUserAnswerValidator;
 import java.util.Scanner;
 
 public class InputView {
-    private final String INPUT_ANSWER_STRING = "숫자를 입력해주세요 : ";
-    private final String INPUT_RESTART_STRING = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private final String INPUT_ANSWER_MESSAGE = "숫자를 입력해주세요 : ";
+    private final String INPUT_RESTART_MESSAGE_FORMAT = "게임을 새로 시작하려면 %s, 종료하려면 %s를 입력하세요.";
+    private final String INPUT_RESTART_MESSAGE = String.format(INPUT_RESTART_MESSAGE_FORMAT,
+            Command.RESTART.getValue(), Command.QUIT.getValue());
     private final Scanner sc;
 
     public InputView() {
@@ -16,7 +19,7 @@ public class InputView {
     }
 
     public BaseballNumbers readInputUserAnswer() {
-        System.out.print(INPUT_ANSWER_STRING);
+        System.out.print(INPUT_ANSWER_MESSAGE);
         try {
             return InputUserAnswerValidator.validate(sc.next());
         } catch (IllegalArgumentException e) {
@@ -26,7 +29,7 @@ public class InputView {
     }
 
     public boolean readInputRestart() {
-        System.out.println(INPUT_RESTART_STRING);
+        System.out.println(INPUT_RESTART_MESSAGE);
         try {
             return InputRestartValidator.validate(sc.next());
         } catch (IllegalArgumentException e) {
