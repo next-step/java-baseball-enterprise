f7512d1c3b3abc4afeec8f377ed82f41ed704707
GrasshopperBears <starday456@naver.com>
2022-12-12 16:17:23 +0900

    test: implement requirements 2 - 괄호가 있는 string에서 괄호를 제거하는 함수 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -20,4 +20,12 @@ public class StringTest {
         String[] actual = source.split(",");
         assertThat(actual).containsExactly("1");
     }
+
+    @Test
+    @DisplayName("test substring")
+    void substring() {
+        String source = "(1,2)";
+        String parenthesisRemoved = source.substring(1, source.length()-1);
+        assertThat(parenthesisRemoved).isEqualTo("1,2");
+    }
 }
