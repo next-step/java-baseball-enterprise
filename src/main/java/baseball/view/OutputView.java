67965dc04c17cb0851bcbfb32771ea135aae5f2b
kyne.k <dd2307@snu.ac.kr>
2022-12-12 20:48:27 +0900

    feat: OutputView.java 구현
    - StringBuilder 활용
    - addString() 메서드를 구현하여 스트라이크, 볼을 StringBuilder에 더하는 과정을 한 번에 처리
    - 스트라이크, 볼에 대한 문자열을 추가한 이후에도 비어있다면 낫싱을 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,4 +1,27 @@
 package baseball.view;
 
+import baseball.domain.StrikeBallCount;
+
 public class OutputView {
+    private final String STRIKE_KOR = "스트라이크 ";
+    private final String BALL_KOR = "볼";
+    private final String NOTHING_KOR = "낫싱";
+    private StringBuilder sb;
+
+    public void printResult(StrikeBallCount strikeBallCount) {
+        sb = new StringBuilder();
+        addString(strikeBallCount.getStrikeCount(), STRIKE_KOR);
+        addString(strikeBallCount.getBallCount(), BALL_KOR);
+        if (sb.length() == 0){
+            sb.append(NOTHING_KOR);
+        }
+        System.out.println(sb.toString().trim());
+    }
+
+    public void addString(int num, String kor_string){
+        if (num > 0){
+            sb.append(num);
+            sb.append(kor_string);
+        }
+    }
 }
