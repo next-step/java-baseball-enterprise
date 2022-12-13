d9bf525d372947cd9c19aa01df6b295259ac450c
Kloong1 <dkdlsmsm98@gmail.com>
2022-12-13 09:45:51 +0900

    feat: 콘솔로 사용자의 3자리 수 입력을 받는 기능 추가
    사용자의 콘솔 입력을 받아 검증한 뒤 파싱해서 반환하는 기능 추가

---
+++ src/main/java/baseball/NumberBaseballUi.java
@@ -0,0 +1,50 @@
+package baseball;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class NumberBaseballUi {
+
+    private BufferedReader reader;
+    private final PlayerInputValidator validator;
+
+    public NumberBaseballUi(PlayerInputValidator validator) {
+        this.validator = validator;
+    }
+
+    public List<Integer> readPlayerBaseballNumber() throws IOException {
+        reader = new BufferedReader(new InputStreamReader(System.in));
+        String playerInput = null;
+        while ((playerInput = readPlayerBaseballNumberString()) == null) {}
+        reader.close();
+        int playerBaseballNumber = Integer.parseInt(playerInput);
+        return extractDigitsInOrder(playerBaseballNumber);
+    }
+
+    private String readPlayerBaseballNumberString() throws IOException {
+        System.out.print("숫자를 입력해주세요: ");
+        String playerInputString = reader.readLine();
+        if (playerInputString != null) {
+            playerInputString = playerInputString.trim();
+        }
+        if (!validator.validateBaseballNumber(playerInputString)) {
+            System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+            return null;
+        }
+        return playerInputString;
+    }
+    private List<Integer> extractDigitsInOrder(int playerBaseballNumber) {
+        List<Integer> digits = new ArrayList<>();
+        while (playerBaseballNumber != 0) {
+            digits.add(playerBaseballNumber % 10);
+            playerBaseballNumber /= 10;
+        }
+        Collections.reverse(digits);
+        return digits;
+    }
+
+}
