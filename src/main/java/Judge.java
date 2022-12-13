be050df55dc5599a8cb9c9fe7c1fba6e1d751c52
Choi Jiwon <c.jiwon3000@gmail.com>
2022-12-13 20:35:16 +0900

    feat: game restart / stop
    Update Judge.java
    - remove constructor
    - replace that function to another method named setOutput()
    - add judge() and restart() methods
    
    Update Main.java
    - create start() method to repeat games
    - change variables local to static

--- src/main/java/Judge.java
+++ src/main/java/Judge.java
@@ -8,24 +8,8 @@ public class Judge {
     int total;
     int strike;
 
-    public Judge(int[] numbers) {
-        List<Integer> input = getInput();
-        int[] result = getStrikeAndBall(numbers, input);
-        StringBuilder sb = new StringBuilder();
-        if (result[0] > 0) {
-            sb.append(result[0] + " 스트라이크 ");
-        }
-        if (result[1] > 0) {
-            sb.append(result[1] + " 볼");
-        }
-        if (result[0] == 0 && result[1] == 0) {
-            sb.append("낫싱");
-        }
-        System.out.println(sb);
-    }
-
     public List<Integer> getInput() {
-        System.out.println("숫자를 입력해주세요 : ");
+        System.out.print("숫자를 입력해주세요 : ");
         Scanner scanner = new Scanner(System.in);
         List<Integer> input = Arrays.stream(scanner.next().split(""))
                 .map(s -> Integer.parseInt(s)).collect(Collectors.toList());
@@ -48,6 +32,34 @@ public class Judge {
         if (input.get(idx) == numbers[idx]) {
             strike++;
         }
+    }
+
+    public String setOutput(int[] result) {
+        StringBuilder sb = new StringBuilder();
+        if (result[0] > 0) {
+            sb.append(result[0] + " 스트라이크 ");
+        }
+        if (result[1] > 0) {
+            sb.append(result[1] + " 볼");
+        }
+        if (result[0] == 0 && result[1] == 0) {
+            sb.append("낫싱");
+        }
+        return sb.toString();
+    }
+
+    public boolean judge(int[] numbers) {
+        List<Integer> input = getInput();
+        int[] result = getStrikeAndBall(numbers, input);
+        System.out.println(setOutput(result));
+        return result[0] == 3;
+    }
+
+    public boolean restart() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        Scanner scanner = new Scanner(System.in);
+        int input = Integer.parseInt(scanner.next());
+        return input == 1;
     }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,26 @@
 
 public class Main {
 
+    static boolean continued;
+    static NumberGenerator numberGenerator = new NumberGenerator(3);
+    static Judge judge = new Judge();
+
     public static void main(String[] args) {
 
-        NumberGenerator numberGenerator = new NumberGenerator(3);
-        Judge judge = new Judge(numberGenerator.getRandomNumbers());
+        continued = true;
+        while (continued) {
+            continued = start();
+        }
 
+    }
+
+    public static boolean start() {
+        boolean isCorrect = false;
+        int[] answer = numberGenerator.getRandomNumbers();
+        while (!isCorrect) {
+            isCorrect = judge.judge(answer);
+        }
+        return judge.restart();
     }
 
 }
