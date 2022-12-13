f5dfe6c91a1bca173da72a22b3ce397799739a35
Kloong1 <dkdlsmsm98@gmail.com>
2022-12-13 10:02:25 +0900

    test: 사용자의 게임 재시작/종료 입력을 받는 기능 테스트 코드 추가

--- src/test/java/baseball/NumberBaseballUiTest.java
+++ src/test/java/baseball/NumberBaseballUiTest.java
@@ -48,4 +48,43 @@ class NumberBaseballUiTest {
         assertThat(out.toString()).hasToString(expectedOutput);
     }
 
+    @Test
+    void readGameRestartInput_성공1() throws IOException {
+        NumberBaseballUi numberBaseballUi = new NumberBaseballUi(new PlayerInputValidator());
+        String input = "1";
+
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertThat(numberBaseballUi.readGameRestartInput()).isEqualTo(1);
+    }
+
+    @Test
+    void readGameRestartInput_성공2() throws IOException {
+        NumberBaseballUi numberBaseballUi = new NumberBaseballUi(new PlayerInputValidator());
+        String input = "2";
+
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertThat(numberBaseballUi.readGameRestartInput()).isEqualTo(2);
+    }
+
+    @Test
+    void readGameRestartInput_실패1() throws IOException {
+        NumberBaseballUi numberBaseballUi = new NumberBaseballUi(new PlayerInputValidator());
+        String input = "asd\n1";
+        String expectedOutput = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n" +
+                "잘못된 입력입니다. 다시 입력해주세요.\n" +
+                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
+
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        assertThat(numberBaseballUi.readGameRestartInput()).isEqualTo(1);
+        assertThat(out.toString()).hasToString(expectedOutput);
+    }
 }
