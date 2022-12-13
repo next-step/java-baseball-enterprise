c6f3e6f9853af6210fecf712cf956f48dd9e6c32
Kloong1 <dkdlsmsm98@gmail.com>
2022-12-13 09:47:10 +0900

    feat: 사용자의 3자리 수 콘솔 입력을 받는 기능을 테스트하는 코드 추가
    사용자 입력 검증 실패시 오류 메시지를 출력하고 다시 입력을 받는 기능까지
    테스트함

---
+++ src/test/java/baseball/NumberBaseballUiTest.java
@@ -0,0 +1,51 @@
+package baseball;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberBaseballUiTest {
+
+    @Test
+    void readPlayerBaseballNumber_성공1() throws IOException {
+        NumberBaseballUi numberBaseballUi = new NumberBaseballUi(new PlayerInputValidator());
+        String input = "123";
+
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertThat(numberBaseballUi.readPlayerBaseballNumber()).containsExactly(1, 2, 3);
+    }
+
+    @Test
+    void readPlayerBaseballNumber_성공2() throws IOException {
+        NumberBaseballUi numberBaseballUi = new NumberBaseballUi(new PlayerInputValidator());
+        String input = "468";
+
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertThat(numberBaseballUi.readPlayerBaseballNumber()).containsExactly(4, 6, 8);
+    }
+
+    @Test
+    void readPlayerBaseballNumber_실패1() throws IOException {
+        NumberBaseballUi numberBaseballUi = new NumberBaseballUi(new PlayerInputValidator());
+        String input = "111\n123";
+        String expectedOutput = "숫자를 입력해주세요: 잘못된 입력입니다. 다시 입력해주세요.\n숫자를 입력해주세요: ";
+
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        assertThat(numberBaseballUi.readPlayerBaseballNumber()).containsExactly(1, 2, 3);
+        assertThat(out.toString()).hasToString(expectedOutput);
+    }
+
+}
