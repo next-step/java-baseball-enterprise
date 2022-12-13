6ee0835093e10a034c9e570e8970f7ce88ae9050
wilgur513 <wilgur513@naver.com>
2022-12-13 23:01:36 +0900

    feat: generate random numbers

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,14 +1,14 @@
 package baseball;
 
-import baseball.model.Numbers;
 import baseball.model.NumbersGenerator;
+import baseball.model.RandomNumbersGenerator;
 import baseball.view.View;
 
 public class Application {
 
     public static void main(String[] args) {
         View view = new View(System.out, System.in);
-        NumbersGenerator numbersGenerator = () -> Numbers.from("123");
+        NumbersGenerator numbersGenerator = new RandomNumbersGenerator();
         BaseBallGame game = new BaseBallGame(view, numbersGenerator);
         game.run();
     }
