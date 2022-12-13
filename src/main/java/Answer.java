b53413011bd0712ac97d0f1d43a87ca625bd034d
SJGeeKo <sungjun.gee.ko@gmail.com>
2022-12-13 16:13:55 +0900

    chore: Answer 일부 메서드 접근제어자 변경
    외부 접근이 불필요한 메서드에 대해 접근제어자 변경
    - setAnswer()
    - addAnswerNumber()
    - pickNumber()
    - validateDuplication()

--- src/main/java/Answer.java
+++ src/main/java/Answer.java
@@ -23,7 +23,7 @@ public class Answer {
     /*
      * 3개의 중복되지 않는 숫자를 답으로 선택하여 answer 리스트에 추가.
      */
-    public void setAnswer() {
+    private void setAnswer() {
         while (answerList.size() < 3) {
             addAnswerNumber();
         }
@@ -33,7 +33,7 @@ public class Answer {
      * 임의의 숫자를 하나 뽑는다.
      * 이미 답으로 선택된 숫자가 아니면 answerList에 추가.
      */
-    public void addAnswerNumber() {
+    private void addAnswerNumber() {
         int num = pickNumber();
         if (validateDuplication(num)) {
             answerList.add(num);
@@ -43,7 +43,7 @@ public class Answer {
     /*
      * 1~9 사이의 임의의 숫자를 뽑아 리턴.
      */
-    public int pickNumber() {
+    private int pickNumber() {
         int pickNum = 0;
         while (pickNum == 0) {
             pickNum = (int) (Math.random() * 10);
@@ -55,7 +55,7 @@ public class Answer {
      * 인수로 받은 숫자가 이미 answerList에 포함됐는지 확인.
      * 중복이 있을 시 false, 없을 시 true 리턴.
      */
-    public boolean validateDuplication(int num) {
+    private boolean validateDuplication(int num) {
         if (answerList.contains(num)) {
             return false;
         }
