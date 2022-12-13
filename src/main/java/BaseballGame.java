cd094ced6bf538ed188f70290da36eda16c23883
GrasshopperBears <starday456@naver.com>
2022-12-13 18:05:10 +0900

    feat: strike, ball 개수 세는 class 구현 - 결과 반환을 위한 JudgementResult 클래스 추가 - 정답과 사용자 입력을 기반으로 strike, ball 개수 count

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,10 +1,12 @@
 public class BaseballGame {
     UserInterface userInterface = new UserInterface();
     NumberGenerator numberGenerator = new NumberGenerator();
+    Judgement judgement = new Judgement();
     int[] answerNumbers;
 
     public void start() {
         answerNumbers = numberGenerator.generate();
         String userInput = userInterface.getUserInput();
+        JudgementResult judgementResult = judgement.judge(answerNumbers, userInput);
     }
 }
---
+++ src/main/java/JudgementResult.java
@@ -0,0 +1,8 @@
+public class JudgementResult {
+    int strikes, balls;
+
+    public JudgementResult(int strikes, int balls) {
+        this.strikes = strikes;
+        this.balls = balls;
+    }
+}
