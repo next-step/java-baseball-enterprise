443e720b02b72138d010ac8acde1ced8a84efda5
fbgnwhife1 <fbgnwhife@gamil.com>
2022-12-15 16:58:54 +0900

    feat: add Computer method

---
+++ src/main/java/Computer.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+
+public class Computer {
+    private final String answer;
+    private final HashMap<Character, Integer> answerPosition;
+
+    public Computer(){
+        answer = makeNumber();
+        answerPosition = new HashMap<>();
+        Common.setPositionNumber(answer, answerPosition);
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public HashMap<Character, Integer> getAnswerPosition() {
+        return answerPosition;
+    }
+
+    private String makeNumber() {
+        int number = (int) (Math.random() * (999 - 99) + 100);
+        String num_string = String.valueOf(number);
+
+        if (!Common.validation(num_string)) {
+            return makeNumber();
+        }
+
+        return num_string;
+    }
+}
+
