ab14eb059d13972718cd08694e41817f69799bb0
Neurumaru <ksj6659@gmail.com>
2022-12-15 14:40:55 +0900

    test(GameUI): add test cases of mapString
    Add test cases of mapString:
    - Change a key to a value.
    - Change multiple same keys to a value.
    - Not change a invalid key.

--- src/test/java/com/neurumaru/baseball/GameUITest.java
+++ src/test/java/com/neurumaru/baseball/GameUITest.java
@@ -16,4 +16,14 @@ public class GameUITest {
         assertThat(gameUI.getString("Test.TestTag")).isEqualTo("Test String");
         assertThat(gameUI.getString("Test.NoExist")).isEqualTo(null);
     }
+
+    @Test
+    void mapStringTest() {
+        Map<String, String> map = new HashMap<>();
+        map.put("{0}", "10");
+        GameUI gameUI = new GameUI(System.in, System.out);
+        assertThat(gameUI.mapString("5 + 5 = {0}", map)).isEqualTo("5 + 5 = 10");
+        assertThat(gameUI.mapString("{0} + {0} = 20", map)).isEqualTo("10 + 10 = 20");
+        assertThat(gameUI.mapString("5 + 5 = {1}", map)).isEqualTo("5 + 5 = {1}");
+    }
 }
