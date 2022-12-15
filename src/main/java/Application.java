e14dafb690986519453c1a093508ba4f46c906f6
fbgnwhife1 <fbgnwhife@gamil.com>
2022-12-15 17:00:25 +0900

    feat: Application start

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,6 +3,12 @@ import java.io.IOException;
 public class Application {
 
     public static void main(String[] args) throws IOException {
+        boolean start = true;
 
+        while(start){
+            Game game = new Game();
+            game.start();
+            start = game.replay();
+        }
     }
 }
