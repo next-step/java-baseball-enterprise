47c3037de817f08e232a1371a4f0ca467208dac2
Neurumaru <ksj6659@gmail.com>
2022-12-15 15:48:57 +0900

    fix(GameUI): change output stream to print stream
    Change OutputStream to PrintStream.
    
    PrintStream is needed for using print or println.

--- src/main/java/com/neurumaru/baseball/GameUI.java
+++ src/main/java/com/neurumaru/baseball/GameUI.java
@@ -1,23 +1,23 @@
 package com.neurumaru.baseball;
 
 import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class GameUI {
     private final Scanner scanner;
-    private final OutputStream outputStream;
+    private final PrintStream printStream;
     private final Map<String, String> stringMap;
 
-    public GameUI(InputStream inputStream, OutputStream outputStream) {
-        this(inputStream, outputStream, new HashMap<>());
+    public GameUI(InputStream inputStream, PrintStream printStream) {
+        this(inputStream, printStream, new HashMap<>());
     }
 
-    public GameUI(InputStream inputStream, OutputStream outputStream, Map<String, String> stringMap) {
+    public GameUI(InputStream inputStream, PrintStream printStream, Map<String, String> stringMap) {
         this.scanner = new Scanner(inputStream);
-        this.outputStream = outputStream;
+        this.printStream = printStream;
         this.stringMap = stringMap;
     }
 
