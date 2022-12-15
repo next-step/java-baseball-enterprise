4ac1e44dac5f154765c1df30507599cb17125263
Neurumaru <ksj6659@gmail.com>
2022-12-15 13:49:55 +0900

    feat(GameUI): add a constructor to get parameter for input and output
    Add a constructor that gets inputStream, outputStream, and stringMap.
    - inputStream is used for scanner, input.
    - outputStream is used for output.
    - stringMap is used to get string with a tag.

---
+++ src/main/java/com/neurumaru/baseball/GameUI.java
@@ -0,0 +1,18 @@
+package com.neurumaru.baseball;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Map;
+import java.util.Scanner;
+
+public class GameUI {
+    private final Scanner scanner;
+    private final OutputStream outputStream;
+    private final Map<String, String> stringMap;
+
+    public GameUI(InputStream inputStream, OutputStream outputStream, Map<String, String> stringMap) {
+        this.scanner = new Scanner(inputStream);
+        this.outputStream = outputStream;
+        this.stringMap = stringMap;
+    }
+}
