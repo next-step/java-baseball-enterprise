77828e1b50c26b1f3c501a8312216c5502d092d5
aaron.j <aaron.j@kakaocorp.com>
2022-12-15 19:39:14 +0900

    refactor: 난수 3개 생성하는 메소드 분리

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -4,9 +4,16 @@ import java.util.Random;
 import java.util.Set;
 
 public class Baseball {
+
+    private static Set<Integer> numbers = new LinkedHashSet<>();
     public static void main(String[] args) {
-        /* 게임을 시작하면 1에서 9까지 서로 다른 임의의 수 3개가 생성된다. */
-        Set<Integer> numbers = new LinkedHashSet<>();
+        create3Number();
+
+    }
+
+    /* 게임을 시작하면 1에서 9까지 서로 다른 임의의 수 3개가 생성된다. */
+    public static void create3Number() {
+        numbers.clear();
         Random random = new Random();
 
         while (numbers.size() < 3) {
--- src/test/java/BaseballTest.java
+++ src/test/java/BaseballTest.java
@@ -11,10 +11,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BaseballTest {
 
+    private Set<Integer> numbers = new LinkedHashSet<>();
+
     @Test
     @DisplayName("게임을 시작하면 1에서 9까지 서로 다른 임의의 수 3개가 생성된다.")
     void create3Number() {
-        Set<Integer> numbers = new LinkedHashSet<>();
+        numbers.clear();
         Random random = new Random();
 
         while (numbers.size() < 3) {
@@ -22,5 +24,11 @@ class BaseballTest {
         }
 
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("사용자에게 3개의 숫자를 입력 받고 입력한 숫자와 임의의 숫자를 비교하여 결과를 출력한다.")
+    void insert3Number() {
+
     }
 }
