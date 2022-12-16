cf37ae68c056bcdd7b2c8a64e27a8bcad76c6bb5
Nelfunction <oat641@gmail.com>
2022-12-16 17:47:42 +0900

    feat: test class file

---
+++ src/test/java/BaseBallGameTest.java
@@ -0,0 +1,45 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BaseBallGameTest {
+    static BaseBallGame baseBallGame;
+
+    @BeforeAll
+    public static void beforeAll(){
+        baseBallGame = new BaseBallGame();
+    }
+
+    @Test
+    void init() {
+    }
+
+    @Test
+    void setNumbers() {
+    }
+
+    @Test
+    void getRandomNumber() {
+    }
+
+    @Test
+    void getInputs() {
+    }
+
+    @Test
+    void compareNumbers() {
+    }
+
+    @Test
+    void compareDigit() {
+    }
+
+    @Test
+    void endgame() {
+    }
+
+    @Test
+    void mainGame() {
+    }
+}
