2ac86a8bcb2bbbd3f0cb9d585a0e019aa685ab15
Seongsu Park <86999506+HolywaterPark@users.noreply.github.com>
2022-12-16 18:25:42 +0900

    feat(): 계속할지 여부 묻기

--- src/main/java/NumberBaseball.java
+++ src/main/java/NumberBaseball.java
@@ -77,6 +77,19 @@ public class NumberBaseball {
         }
         System.out.print("\n");
     }
+
+    public boolean print_end_message(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        while(true) {
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+            Scanner scanner = new Scanner(System.in);
+            int input = Integer.parseInt(scanner.nextLine());
+            if (input == CONTINUE) return true;
+            if (input == END) return false;
+            System.out.println("잘못 입력하셨습니다");
+        }
+    }
+
     public boolean do_game() {
         initialize_parameter();
         generate_random_numbers();
