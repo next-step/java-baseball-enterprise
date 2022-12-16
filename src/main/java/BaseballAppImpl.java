f1f6e7837b658bf8a70d95d5c8832fdda5531c83
yooj929 <jang0929@naver.com>
2022-12-16 18:02:04 +0900

    feat(BaseballApp, BaseballGame) 전체 적인 기능 구현

--- src/main/java/BaseballAppImpl.java
+++ src/main/java/BaseballAppImpl.java
@@ -1,14 +1,22 @@
+import config.BaseballConfig;
 import domain.BaseballGame;
 import domain.BaseballGameImpl;
 
 public class BaseballAppImpl implements BaseballApp {
-    private final BaseballGame baseballGame;
+    private BaseballConfig baseballConfig;
 
-    public BaseballAppImpl(BaseballGame baseballGame){
-        this.baseballGame = baseballGame;
+    public BaseballAppImpl(BaseballConfig baseballConfig) {
+        this.baseballConfig = baseballConfig;
     }
+
     @Override
     public void run() {
+        BaseballGame baseballGame = new BaseballGameImpl(baseballConfig);
         baseballGame.play(1);
+    }
+
+    public static void main(String[] args) {
+        BaseballApp baseballApp = new BaseballAppImpl(new BaseballConfig());
+        baseballApp.run();
     }
 }
