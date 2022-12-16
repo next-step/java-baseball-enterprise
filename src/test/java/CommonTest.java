43f7ba8534815988edb2cdb283d8cebd0d165da1
fbgnwhife1 <fbgnwhife@gamil.com>
2022-12-17 02:46:33 +0900

    test: add test

--- src/test/java/CommonTest.java
+++ src/test/java/CommonTest.java
@@ -8,12 +8,13 @@ public class CommonTest {
 
     @Test
     void validationTest(){
-        assertFalse(common.validation("0123"));
-        assertFalse(common.validation("111"));
-        assertFalse(common.validation("103"));
-        assertFalse(common.validation("979"));
-        assertTrue(common.validation("475"));
-        assertTrue(common.validation("123"));
+        assertFalse(common.validation("122"));      // 뒷 두 자리 테스트
+        assertFalse(common.validation("0123"));     // 4자리, 0 포함
+        assertFalse(common.validation("113"));      // 앞 두 자리 테스트
+        assertFalse(common.validation("103"));      // 0 포함 테스트
+        assertFalse(common.validation("979"));      // 연속x 중복 o
+        assertTrue(common.validation("475"));       // 통과
+        assertTrue(common.validation("123"));       //통과
     }
 }
 
