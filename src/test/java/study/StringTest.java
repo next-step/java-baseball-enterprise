5ba001852e05deb571c418221f4b5d6099bef0d8
siryeong <siryeongchoi@gmail.com>
2022-12-16 17:58:57 +0900

    test(study): Junit5 학습 테스트
    Junit5 학습 테스트
    - String test: 문자열 분리, trim, exception 유닛 테스트 학습
    - Set test : Set자료구조 size, contains 유닛 테스트 학습
    - parameterized test 학습

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,9 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
@@ -10,4 +11,31 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+	@Test
+	void split() {
+		String[] oneTwo = "1,2".split(",");
+		assertThat(oneTwo).contains("1", "2");
+
+		String[] one = "1".split(",");
+		assertThat(one).containsExactly("1");
+	}
+
+	@Test
+	void trimBracket() {
+		String bracket = "(1,2)";
+		String trim = bracket.substring(1, bracket.length()-1);
+		assertThat(trim).isEqualTo("1,2");
+	}
+
+	@Test
+	@DisplayName("문자열 인덱스 범위 초과시 exception 확인")
+	void exceptionThrow() {
+		String abc = "abc";
+		assertThatThrownBy(()-> {
+			abc.charAt(10);
+		})
+			.isInstanceOf(StringIndexOutOfBoundsException.class)
+			.hasMessageContaining("out of bounds");
+	}
 }
