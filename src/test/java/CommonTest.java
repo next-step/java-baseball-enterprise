8864c6b0c7aec2e3ea3883f95b61e2577d0bdb10
fbgnwhife1 <fbgnwhife@gamil.com>
2022-12-17 02:55:05 +0900

    refactor: refactoring validation test

--- src/test/java/CommonTest.java
+++ src/test/java/CommonTest.java
@@ -4,17 +4,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class CommonTest {
 
-    private final Common common = new Common();
-
     @Test
     void validationTest(){
-        assertFalse(common.validation("122"));      // 뒷 두 자리 테스트
-        assertFalse(common.validation("0123"));     // 4자리, 0 포함
-        assertFalse(common.validation("113"));      // 앞 두 자리 테스트
-        assertFalse(common.validation("103"));      // 0 포함 테스트
-        assertFalse(common.validation("979"));      // 연속x 중복 o
-        assertTrue(common.validation("475"));       // 통과
-        assertTrue(common.validation("123"));       //통과
+        assertFalse(Common.validation("122"));      // 뒷 두 자리 테스트
+        assertFalse(Common.validation("0123"));     // 4자리, 0 포함
+        assertFalse(Common.validation("113"));      // 앞 두 자리 테스트
+        assertFalse(Common.validation("103"));      // 0 포함 테스트
+        assertFalse(Common.validation("979"));      // 연속x 중복 o
+        assertTrue(Common.validation("475"));       // 통과
+        assertTrue(Common.validation("123"));       //통과
     }
 }
 
