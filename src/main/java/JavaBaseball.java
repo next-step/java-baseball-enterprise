0b5494d70500f5777ca22bf5a38caeb038e394e1
SJGeeKo <sungjun.gee.ko@gmail.com>
2022-12-16 12:09:21 +0900

    feat: 입력값 채점기능 추가
    채점 타입 구분을 위한 ScoreType enum 추가

--- src/main/java/JavaBaseball.java
+++ src/main/java/JavaBaseball.java
@@ -1,8 +1,89 @@
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class JavaBaseball {
+    static final int THREE_STRIKE = 3;
+    static final int RESTART_GAME = 1;
+    static final int END_GAME = 2;
+    
     static Answer answer = new Answer();
+    static HashMap<ScoreType, Integer> scoreMap = new HashMap<>();
+    static HashMap<Integer, Integer> userInput = new HashMap<>();
+    
+    static boolean continueGame = true;
+
     public static void main(String[] args) {
+        runBaseBall();
+    }
+
+    /*
+     * 숫자야구 실행
+     */
+    static void runBaseBall() {
         
+    }
+
+    /*
+     * scoreMap을 초기화함
+     */
+    private static void initScoreMap() {
+        scoreMap.put(ScoreType.S, 0);
+        scoreMap.put(ScoreType.B, 0);
+        scoreMap.put(ScoreType.N, 0);
+    }
+
+    /*
+     * 유저 입력값에 대해 채점
+     */
+    static void score() {
+        for (Integer i : userInput.keySet()) {
+            checkNumber(i);
+        }
+        printScore();
+    }
+
+    /*
+     * 채점 결과 터미널에 출력
+     */
+    private static void printScore() {
+        String scoreStr = "";
+        if (scoreMap.get(ScoreType.S) != 0) {
+            scoreStr += scoreMap.get(ScoreType.S) + " 스트라이크 ";
+        }
+        if (scoreMap.get(ScoreType.B) != 0) {
+            scoreStr += scoreMap.get(ScoreType.B) + " 볼 ";
+        }
+        System.out.println(scoreStr);
+    }
+
+    /*
+     * 하나의 숫자에 대해 채점
+     */
+    static void checkNumber(int num) {
+        if (answer.contains(num)) {
+            checkStrikeOrBall(num);
+            return;
+        }
+        scoreMap.put(ScoreType.N, scoreMap.get(ScoreType.N) + 1);
+    }
+
+    /*
+     * 존재가 확인된 숫자에 대해 스트라이크인지 볼인지 확인 후 scoreMap에 반영
+     */
+    private static void checkStrikeOrBall(int num) {
+        if (answer.getIndex(num) == userInput.get(num)) {
+            scoreMap.put(ScoreType.S, scoreMap.get(ScoreType.S) + 1);
+            return;
+        }
+        scoreMap.put(ScoreType.B, scoreMap.get(ScoreType.B) + 1);
+    }
+
+    /*
+     * 3스트라이크(정답) 여부 판단
+     */
+    private static boolean isThreeStrike() {
+        if (scoreMap.get(ScoreType.S) == THREE_STRIKE) {
+            return true;
+        }
+        return false;
     }
 }
---
+++ src/main/java/ScoreType.java
@@ -0,0 +1,5 @@
+public enum ScoreType {
+    S,
+    B,
+    N
+}
