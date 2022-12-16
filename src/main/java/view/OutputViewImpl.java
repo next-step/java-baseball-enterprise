532fc5fcc3e722c35789c6f3182c6d15b06b2ba1
yooj929 <jang0929@naver.com>
2022-12-16 18:05:46 +0900

    feat : 입력을 위한 출력 내용 구현

--- src/main/java/view/OutputViewImpl.java
+++ src/main/java/view/OutputViewImpl.java
@@ -7,4 +7,19 @@ public class OutputViewImpl implements OutputView {
     public void printStrikeAndBall(JudgeResult judgeResult) {
         System.out.println(judgeResult.toString());
     }
+
+    @Override
+    public void printInputThreeNumber() {
+        System.out.println("숫자를 입력해주세요 : ");
+    }
+
+    @Override
+    public void printGameResult() {
+        System.out.println("3개의 숫자를 모두 맞히였습니다! 게임 종료");
+    }
+
+    @Override
+    public void printInputGameOption() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려 2를 입력하세요.");
+    }
 }
