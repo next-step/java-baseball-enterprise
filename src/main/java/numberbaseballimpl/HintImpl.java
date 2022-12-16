89ea9b8d06b8ee5959f4956d71405f60e953a29d
pluto.yun <pluto.yun@kakaocorp.com>
2022-12-16 14:53:16 +0900

    feat: Hint

--- src/main/java/numberbaseballimpl/HintImpl.java
+++ src/main/java/numberbaseballimpl/HintImpl.java
@@ -4,31 +4,49 @@ import numberbaseball.*;
 
 public class HintImpl implements Hint {
 
-    private HintImpl() {
+    public static final int NUMBERS_SIZE = 3;
+    private int strike = 0;
+    private int ball = 0;
 
+    private HintImpl(Base base, Ball ball) {
+        int[] numbers = ball.getNumbers();
+        for (int i = 0; i < NUMBERS_SIZE; ++i) {
+            increaseHint(base, i, numbers[i]);
+        }
+    }
+
+    private void increaseHint(Base base, int index, int number) {
+        if (base.isStrike(index, number)) {
+            ++strike;
+        }
+        if (base.isBall(index, number)) {
+            ++ball;
+        }
     }
 
     public static Hint create(Base base, Ball ball) {
-        return new HintImpl();
+        if (base == null || ball == null)
+            throw new IllegalArgumentException("Base or ball must be not null.");
+        return new HintImpl(base, ball);
     }
 
     @Override
     public int getStrike() {
-        return 0;
+        return strike;
     }
 
     @Override
     public int getBall() {
-        return 0;
+        return ball;
     }
 
     @Override
     public boolean isNothing() {
-        return false;
+        return strike + ball == 0;
     }
 
     @Override
     public boolean isAnswer() {
-        return false;
+        return strike == NUMBERS_SIZE;
     }
 }
