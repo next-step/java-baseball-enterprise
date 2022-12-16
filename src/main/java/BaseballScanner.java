28e75881ab05c2a5e6b1cf398f6e731c985e91d2
SJGeeKo <sungjun.gee.ko@gmail.com>
2022-12-16 12:01:33 +0900

    refactor: BaseballScanner를 ArrayList에서 HashMap 기반으로 변경
    Answer와 동일하게 HashMap 구조로 변경

--- src/main/java/BaseballScanner.java
+++ src/main/java/BaseballScanner.java
@@ -1,27 +1,25 @@
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class BaseballScanner {
 
     static Scanner sc = new Scanner(System.in);
+    static HashMap<Integer, Integer> map = new HashMap<>();
 
     /*
      * 유저로부터 값을 입력 받고 각 자릿수 숫자들로 구성된 ArrayList를 반환
      */
-    public static ArrayList<Integer> getUserInputNumberList() {
-        ArrayList<Integer> list = new ArrayList<>();
-        boolean inputFlag = false;
-
+    public static HashMap<Integer, Integer> getUserInputNumberMap() {
         System.out.print("숫자를 입력해주세요: ");
-        String userInputStr = sc.nextLine();
+        String userInputStr = sc.nextLine().trim();
 
-        //TODO: 입력값 유효성 검증 로직 필요
-        
+        // TODO: 입력값 유효성 검증 로직 필요
+
         for (int i = 0; i < userInputStr.length(); i++) {
             int num = Character.getNumericValue(userInputStr.charAt(i));
-            list.add(num);
+            map.put(num, i);
         }
 
-        return list;
+        return map;
     }
 }
