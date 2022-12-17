11202c8ccf92bde08a8b4ff60302e11447ed50da
SJGeeKo <sungjun.gee.ko@gmail.com>
2022-12-18 00:02:56 +0900

    test: Answer 리팩토링, ScoreBoard 테스트 코드 추가
    resetAnswer 메서드 관련 테스트 코드 삭제
    
    contains 테스트 코드 리팩토링

--- src/test/java/AnswerTest.java
+++ src/test/java/AnswerTest.java
@@ -1,13 +1,10 @@
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
-import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 public class AnswerTest {
@@ -27,44 +24,17 @@ public class AnswerTest {
         assertFalse(zeroFlag);
     }
 
-    // 사실상 위 테스트와 동일함. 의미가 있는지 의문.
-    @Test
-    void resetAnswer() {
-        Answer answer = new Answer();
-
-        answer.resetAnswer();
-
-        // answerMap에는 3개의 항목이 포함된다
-        HashMap<Integer, Integer> answerMap = answer.getAnswerMap();
-        assertEquals(3, answerMap.size());
-
-        // answerMap에는 0이 포함되지 않는다
-        boolean zeroFlag = false;
-        if (answerMap.containsKey(0)) {
-            zeroFlag = true;
-        }
-        assertFalse(zeroFlag);
-    }
-
     @Test
     void contains() {
-        Answer answer = new Answer();
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 0; i < array.length; i++) {
-            list.add(array[i]);
-        }
+        Answer answer = Answer.forTestCode(1, 2, 3);
 
-        for (Integer answerNum : answer.getAnswerMap().keySet()) {
-            // answerMap에 포함된 숫자들은 contains에서 true를 리턴한다
-            assertTrue(answer.contains(answerNum));
-            list.remove(answerNum);
-        }
+        assertTrue(answer.contains(1));
+        assertTrue(answer.contains(2));
+        assertTrue(answer.contains(3));
 
-        for (Integer wrongNum : list) {
-            // answerMap에 포함되지 않은 숫자들은 contains에서 false를 리턴한다
-            assertFalse(answer.contains(wrongNum));
-        }
+        assertFalse(answer.contains(4));
+        assertFalse(answer.contains(5));
+        assertFalse(answer.contains(6));
     }
 
     @Test
