360024e7767da8722a1cc7c0c6ad5849f7a4de5e
SJGeeKo <sungjun.gee.ko@gmail.com>
2022-12-17 23:56:54 +0900

    refactor: Game 내 askRestart를 테스트에 적합한 형태로 변경
    askRestart가 int 인자를 받고 boolean 값을 리턴하는 형태로 변경
    
    scoreBoard는 변경되지 않으므로 final 예약어 추가
    
    printScore가 Util 클래스로 이동하였으므로 해당 코드 변경

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -3,9 +3,9 @@ import java.util.HashMap;
 public class Game {
     static final int END_GAME = 2;
 
-    private ScoreBoard scoreBoard;
+    private final ScoreBoard scoreBoard;
     private HashMap<Integer, Integer> userInput;
-    
+
     private boolean continueGame;
 
     public Game() {
@@ -27,26 +27,25 @@ public class Game {
      * 게임 시작
      */
     public void play() {
-        System.out.println(scoreBoard.getAnswer());
+//        System.out.println(scoreBoard.getAnswer());
         while (!scoreBoard.isThreeStrike()) {
             userInput = Util.getNumberInput();
             scoreBoard.checkScore(userInput);
-            scoreBoard.printScore();
+            Util.printScore(scoreBoard);
         }
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        askRestart();
+        continueGame = askRestart(Util.getRestartInput());
     }
 
     /*
      * 게임 종료 후 재시작 여부 확인
      * 재시작하는 경우 정답(3자리 숫자) 재설정
      */
-    private void askRestart() {
-        int restartInput = Util.getRestartInput();
-        if (restartInput == END_GAME) {
-            continueGame = false;
-            return;
+    boolean askRestart(int input) {
+        if (input == END_GAME) {
+            return false;
         }
         scoreBoard.reset();
+        return true;
     }
 }
