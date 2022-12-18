498e33b3fc66f7b426e1a8f4a88c698707862dc0
Neurumaru <ksj6659@gmail.com>
2022-12-18 16:09:43 +0900

    feat(BaseballConfig): add a class to save config constants
    Save config constants:
    - DIGIT_DEFAULT
    - KOREAN_FILENAME
    - RESTART_NUMBER
    - QUIT_NUMBER

---
+++ src/main/java/com/neurumaru/baseball/BaseballConfig.java
@@ -0,0 +1,8 @@
+package com.neurumaru.baseball;
+
+public class BaseballConfig {
+    public static final int DIGIT_DEFAULT = 3;
+    public static final String KOREAN_FILENAME = "Korean.xml";
+    public static final int RESTART_NUMBER = 1;
+    public static final int QUIT_NUMBER = 2;
+}
