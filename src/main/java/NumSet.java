e8fc9f065f683510ec74f71912d4049635d8619e
winnie.ka <winnie.ka@kakaocorp.com>
2022-12-18 17:15:35 +0900

    feat: 1~9의 서로 다른 임의의 숫자 3개 생성
    NumSet이 1~9 숫자 3개를 저장하는 int[]를 가짐
    NumSet의 생성자에서 int[]에 랜덤 숫자 생성하여 저장

--- src/main/java/NumSet.java
+++ src/main/java/NumSet.java
@@ -1,4 +1,27 @@
+import java.util.Random;
+
 public class NumSet {
+    private int[] numArr = new int[3];
+
+    public NumSet() {
+        Random random = new Random();
+        for (int i = 0; i < 3; i++) {
+            while (true) {
+                numArr[i] = random.nextInt(1, 10);
+                if (isDiffFromForward(i))
+                    break;
+            }
+        }
+    }
+
+    private boolean isDiffFromForward(int idx) {
+        for (int i = 0; i < idx; i++) {
+            if (numArr[idx] == numArr[i])
+                return false;
+        }
+        return true;
+    }
+
     public static int[] convertToList(int num) {
         return new int[]{
                 num / 100,
