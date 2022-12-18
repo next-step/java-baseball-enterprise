aa2fb677f1aa79722fd9c409529ef202d01360ed
SJGeeKo <sungjun.gee.ko@gmail.com>
2022-12-18 18:21:01 +0900

    test: test study 코드 추가
    학습테스트를 통해 JUnit 학습하기 pdf 파일의 요구사항을 구현함

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,52 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split() {
+        //given
+        String str = "1,2";
+        //when
+        String[] arr = str.split(",");
+        //then
+        assertThat(arr).contains("1");
+        assertThat(arr).contains("2");
+    }
+
+    @Test
+    void split2() {
+        //given
+        String str = "1";
+        //when
+        String[] arr = str.split(",");
+        //then
+        assertThat(arr).containsExactly("1");
+    }
+
+    @Test
+    void substring() {
+        String str = "(1,2)";
+        String result = str.substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("StringIndexOutOfBoundsException 테스트")
+    void charAt() {
+        String str = "abc";
+        assertThatThrownBy(() -> str.charAt(4))
+                .isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 4");
     }
 }
