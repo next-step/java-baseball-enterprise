03a1b33e75afdb1eaf0cc41b8da08e0bbea6f544
ekzm8523 <mjw8523@gmail.com>
2022-12-19 00:48:15 +0900

    refactor: baseballNumbers -> numbers 네이밍 변경 & Array -> ArrayList로 자료구조 변경

--- src/main/java/baseball/domain/BaseballNumber.java
+++ src/main/java/baseball/domain/BaseballNumber.java
@@ -1,6 +1,5 @@
 package baseball.domain;
 
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -8,7 +7,7 @@ import java.util.List;
 public class BaseballNumber {
 
     private final List<Integer> candidateNumbers = new ArrayList<>();
-    private final int[] baseballNumbers = new int[3];
+    private final List<Integer> numbers = new ArrayList<>();
 
     public BaseballNumber() {
         for (int i = 1; i <= 9; i++) {
@@ -17,9 +16,14 @@ public class BaseballNumber {
     }
 
     public void GenerateBaseballNumbers() {
+        numbers.clear();
         Collections.shuffle(candidateNumbers);
         for (int i = 0; i <= 2; i++) {
-            baseballNumbers[i] = candidateNumbers.get(i);
+            numbers.add(candidateNumbers.get(i));
         }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 }
