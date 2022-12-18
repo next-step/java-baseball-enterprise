def719f3e661231982febbe8c4d8fe4fdbf6bfd3
Sangyups <feels4637@gmail.com>
2022-12-18 17:06:45 +0900

    feat: 게임 시작 시 정답 생성 기능 추가

--- src/main/java/NumberBaseball.java
+++ src/main/java/NumberBaseball.java
@@ -1,12 +1,31 @@
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import utils.RandomUtil;
 
 public class NumberBaseball {
 
     private List<Integer> answer;
+    private final int digit = 3;
+
+    private boolean isValidAnswer(List<Integer> numbers) {
+        Set<Integer> numSet = new HashSet<>(numbers);
+
+        return numSet.size() == numbers.size();
+    }
+
+    private void setAnswer() {
+        List<Integer> randomNumbers;
+        do {
+            randomNumbers = RandomUtil.generateRandomNumbers(digit);
+            randomNumbers.forEach(System.out::println);
+            System.out.println();
+        } while (!isValidAnswer(randomNumbers));
+
+        this.answer = randomNumbers;
+    }
 
     public void start() {
-        int digit = 3;
-        this.answer = RandomUtil.generateRandomNumbers(digit);
+        setAnswer();
     }
 }
