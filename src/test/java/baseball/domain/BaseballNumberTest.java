9a0943c8525e28839a2f3a0fc49332ee01194d88
ekzm8523 <mjw8523@gmail.com>
2022-12-19 00:48:51 +0900

    test: domain 생성되는 숫자 범위 테스트

--- src/test/java/baseball/domain/BaseballNumberTest.java
+++ src/test/java/baseball/domain/BaseballNumberTest.java
@@ -1,2 +1,31 @@
-package baseball.domain;public class BaseballNumberTest {
+package baseball.domain;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BaseballNumberTest {
+
+    BaseballNumber baseballNumber;
+
+    @BeforeEach
+    public void beforeEach() {
+        baseballNumber = new BaseballNumber();
+    }
+
+    @DisplayName("생성되는 숫자의 범위는 1-9이다.")
+    @Test
+    void GenerateBaseballNumbersTest() {
+        // given
+        baseballNumber.GenerateBaseballNumbers();
+
+        // when
+        List<Integer> numbers = baseballNumber.getNumbers();
+
+        // then
+        assertThat(numbers.stream().allMatch(v -> 1 <= v && v <= 9)).isTrue();
+
+    }
 }
