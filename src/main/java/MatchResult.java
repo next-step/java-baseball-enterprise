8dcf229354a89729a191e3986b42f93d21d420ba
winnie.ka <winnie.ka@kakaocorp.com>
2022-12-18 20:49:52 +0900

    feat: 스트라이크 / 볼 / 낫싱 판별
    MatchResult에 스트라이크/볼 개수 카운트해서 저장
    스크라이크와 볼이 없으면 낫싱

---
+++ src/main/java/MatchResult.java
@@ -0,0 +1,8 @@
+public class MatchResult {
+    private int strike, ball;
+
+    public MatchResult(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+}
--- src/main/java/NumSet.java
+++ src/main/java/NumSet.java
@@ -22,6 +22,37 @@ public class NumSet {
         return true;
     }
 
+    // 사용자가 '112'처럼 중복 숫자를 입력할 때
+    // 스트라이크와 볼이 중복 카운트되지 않도록
+    // 포함하는 수 카운트에서 스트라이크를 빼서 볼을 계산
+    public MatchResult match(int[] userArr) {
+        int strike = countStrike(userArr);
+        int ball = countContains(userArr) - strike;
+        return new MatchResult(strike, ball);
+    }
+
+    private int countStrike(int[] userArr) {
+        int strike = 0;
+        for (int i = 0; i < 3; i++) {
+            if (numArr[i] == userArr[i])
+                strike++;
+        }
+        return strike;
+    }
+
+    private int countContains(int[] userArr) {
+        int contains = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (numArr[i] == userArr[j]) {
+                    contains++;
+                    break;
+                }
+            }
+        }
+        return contains;
+    }
+
     public static int[] convertToList(int num) {
         return new int[]{
                 num / 100,
