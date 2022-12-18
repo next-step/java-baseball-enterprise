c1d6fbed1adce248d94e87a212e1b97a8288aa7e
oscar <oscar.award@kakaocorp.com>
2022-12-18 16:51:35 +0900

    feat: 정답이 나올 때까지 추측 반복 기능

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -4,21 +4,14 @@ import domain.ballnumber.BallResult;
 
 public class OutputView {
 
-    private StringBuilder sb;
-
-    public OutputView() {
-        this.sb = new StringBuilder();
-    }
     public void showBallResult(BallResult ballResult) {
-        if(ballResult.getStrike()>0) {
+        StringBuilder sb = new StringBuilder();
+        if(ballResult.getStrike()>0)
             sb.append(ballResult.getStrike()).append(" 스트라이크 ");
-        }
-        if(ballResult.getBall()>0) {
+        if(ballResult.getBall()>0)
             sb.append(ballResult.getBall()).append(" 볼");
-        }
-        if(sb.length()==0) {
+        if(sb.length()==0)
             sb.append("낫싱");
-        }
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 }
