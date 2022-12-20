0a31e420bfce7010573a34fe5132d722a359ef58
HQkim <hyunkyu001@gmail.com>
2022-12-21 00:32:00 +0900

    feat: 플레이어 수 3개 입력 구현

---
+++ src/main/java/View.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class View {
+    private Scanner sc = new Scanner(System.in);
+
+    public Integer[] getPlayerInput() {
+        System.out.println("숫자를 입력해주세요 : ");
+        String input = sc.nextLine();
+        String[] numbersInString = input.split("");
+        Integer[] numbers = new Integer[3];
+        for (int i = 0; i < 3; i++) {
+            numbers[i] = Integer.parseInt(numbersInString[i]);
+        }
+        return numbers;
+    }
+
+
+}
