b2d0605f262302ad8101f11169713526747c2943
person003333 <person00333@gmail.com>
2022-12-20 15:16:29 +0900

    refactor: User 클래스 테스트
    테스트 조건 추가

--- src/test/java/domain/UserTest.java
+++ src/test/java/domain/UserTest.java
@@ -1,5 +1,6 @@
 package domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,7 +8,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserTest {
     @Test
-    void create(){
+    @DisplayName("중복되지 않는 숫자 테스트")
+    void testUniqueness(){
         User user = new User();
         user.setNumbers("123");
         String actual = user.getNumbers().toString();
@@ -16,6 +18,47 @@ public class UserTest {
         user.reset();
         try {
             user.setNumbers("111");
+        } catch (IllegalArgumentException e) {
+            actual = e.getMessage();
+        }
+        assertThat(actual).isEqualTo("잘못된 입력입니다.");
+    }
+
+    @Test
+    @DisplayName("숫자 중복 입력")
+    void testNotUniqueness(){
+        User user = new User();
+        String actual = "";
+        user.reset();
+        try {
+            user.setNumbers("112");
+        } catch (IllegalArgumentException e) {
+            actual = e.getMessage();
+        }
+        assertThat(actual).isEqualTo("잘못된 입력입니다.");
+    }
+
+    @Test
+    @DisplayName("숫자 초과 입력")
+    void testOverNumber(){
+        User user = new User();
+        String actual = "";
+        user.reset();
+        try {
+            user.setNumbers("1234");
+        } catch (IllegalArgumentException e) {
+            actual = e.getMessage();
+        }
+        assertThat(actual).isEqualTo("잘못된 입력입니다.");
+    }
+    @Test
+    @DisplayName("문자 입력")
+    void testChar(){
+        User user = new User();
+        String actual = "";
+        user.reset();
+        try {
+            user.setNumbers("a12");
         } catch (IllegalArgumentException e) {
             actual = e.getMessage();
         }
