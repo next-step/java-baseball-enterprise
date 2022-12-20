d31d23872898fe45c9c6c8765e5e425ff73bfebc
person003333 <person00333@gmail.com>
2022-12-20 13:43:26 +0900

    feat: Score 클래스 작성
    점수를 기록하는 Score클래스

---
+++ src/main/java/domain/Score.java
@@ -0,0 +1,49 @@
+package domain;
+
+public class Score {
+    private Integer strike;
+    private Integer ball;
+
+    public Score() {
+        reset();
+    }
+
+    public void setStrike(Integer strike) {
+        this.strike = strike;
+    }
+
+    public void setBall(Integer ball) {
+        this.ball = ball;
+    }
+
+    public Integer getStrike() {
+        return strike;
+    }
+
+    public Integer getBall() {
+        return ball;
+    }
+
+    public boolean isFinish(){
+        return this.strike == 3;
+    }
+
+    public String getScore(){
+        StringBuilder sb = new StringBuilder();
+        if (sb.length() == 0){
+            return "아웃";
+        }
+        if (this.ball > 0){
+            sb.append(this.ball).append("볼");
+        }
+        if (this.strike > 0){
+            sb.append(this.strike).append("스트라이크");
+        }
+        return sb.toString();
+    }
+
+    public void reset() {
+        this.strike = 0;
+        this.ball = 0;
+    }
+}
