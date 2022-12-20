07123135d756fd8ba8e821ed94ec4b4a9a2c5e95
HQkim <hyunkyu001@gmail.com>
2022-12-21 01:41:51 +0900

    feat: 점수에 따라 출력하는 기능 추가

--- src/main/java/View.java
+++ src/main/java/View.java
@@ -14,4 +14,12 @@ public class View {
         return numbers;
     }
 
+    public void printScore(int[] scores) {
+        String strike = "";
+        String ball = "";
+        if (scores[0] != 0) strike = scores[0] + "스트라이크";
+        if (scores[1] != 0) ball = scores[1] + "볼";
+        if (scores[0]+scores[1] != 0) System.out.println(strike + " " + ball);
+        if (scores[0]+scores[1] == 0) System.out.println("낫싱");
+    }
 }
