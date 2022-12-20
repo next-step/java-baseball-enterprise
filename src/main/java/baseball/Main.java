c13dc7efad3263cc9c0feaf2321b20759d0840dc
엄태호 <qqsscc7342@gmail.com>
2022-12-20 18:42:08 +0900

    feat: 게임 실행 콘솔 구현

--- src/main/java/baseball/Main.java
+++ src/main/java/baseball/Main.java
@@ -1,2 +1,10 @@
-package baseball;public class Main {
+package baseball;
+
+import baseball.domain.Game;
+
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.Start();
+    }
 }
--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -1,28 +1,64 @@
-package baseball;
+package baseball.domain;
 
+import baseball.domain.Computer;
+import baseball.domain.Result;
+import baseball.util.validator.UserInputValidator;
+
+import javax.xml.validation.Validator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Game {
 
     private Computer computer;
     private Result result;
+    private UserInputValidator validator;
 
     public void Start() {
         initializeProps();
-        Scanner scanner = new Scanner(System.in);
-        int input = 0;
-
-        while(true) {
-            System.out.println("숫자를 입력주세요 : ");
-            input = scanner.nextInt();
-
+        boolean isEnd = false;
+        while(!isEnd) {
+            Scanner scanner = new Scanner(System.in);
+            String resultString = getResult(scanner);
+            System.out.println(resultString);
+            isEnd = determineEndGame(scanner, resultString);
         }
+    }
+
+    private String getResult(Scanner scanner) {
+        return computer.calculate(getIntegerListFromInput(validator.validateInput(getInputFromUser(scanner))))
+                .exportResultString();
+    }
+
+    private Boolean determineEndGame(Scanner scanner, String resultString) {
+        if(resultString.equals("3 스트라이크")) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            return validator.validateEndInputFromUser(scanner.nextLine());
+        }
+        return false;
+    }
+
+    private List<Integer> getIntegerListFromInput(int input) {
+        List<Integer> result = new ArrayList<>();
+        while(input > 0) {
+            result.add(input % 10);
+            input /= 10;
+        }
+        Collections.reverse(result);
+        return result;
+    }
+
+    private String getInputFromUser(Scanner scanner) {
+        System.out.print("숫자를 입력해주세요 : ");
+        return scanner.nextLine();
     }
 
     private void initializeProps() {
         this.computer = new Computer();
         this.result = new Result(0, 0);
+        this.validator = new UserInputValidator();
     }
-
-
 }
