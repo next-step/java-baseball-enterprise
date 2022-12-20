18b86d59e7b2f5814bdb14c8e94b2547f0680069
person003333 <person00333@gmail.com>
2022-12-20 13:29:54 +0900

    refactor: User 클래스 변수명 수정
    Computer 클래스와 똑같이 numbers로 변경

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -5,25 +5,25 @@ import java.util.ArrayList;
 
 public class User {
 
-    private final ArrayList<Integer> number;
+    private final ArrayList<Integer> numbers;
 
-    public ArrayList<Integer> getNumber() {
-        return number;
+    public ArrayList<Integer> getNumbers() {
+        return numbers;
     }
 
 
     public User() {
-        this.number = new ArrayList<>();
+        this.numbers = new ArrayList<>();
     }
 
     public void reset(){
-        this.number.clear();
+        this.numbers.clear();
     }
 
     public void userNumAdd(int num){
-        if(!this.number.contains(num))
+        if(!this.numbers.contains(num))
         {
-            this.number.add(num);
+            this.numbers.add(num);
         }
     }
 
@@ -33,14 +33,14 @@ public class User {
             this.userNumAdd(userInput.charAt(i) - '0');
         }
 
-        if (this.getNumber().size() != 3) {
+        if (this.getNumbers().size() != 3) {
             this.reset();
             throw new IllegalArgumentException("잘못된 입력입니다.");
         }
 
     }
 
-    public void setNumber(String num){
+    public void setNumbers(String num){
 
         if (num.length() != 3 || !isNumeric(num)){
             this.reset();
