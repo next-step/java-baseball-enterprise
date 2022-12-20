7e2c9700ff7169b78c3272040629150d5a9bf46e
eddie.kk <eddie.kk@kakaocorp.com>
2022-12-20 17:28:16 +0900

    feat: 사용자의 수와 컴퓨터의 수를 비교하는 BaseballGame 도메인 구현 - 수가 같은지 다른지 판별 - 수가 같은 자리에 있는지, 포함하고 있는지 판별 - 게임 종료(3 strike) 야부 판별

---
+++ src/main/java/domain/BaseballGame.java
@@ -0,0 +1,39 @@
+package domain;
+
+public class BaseballGame {
+
+    private final String computerNumbers;
+
+    private GameResult currentGameResult;
+
+    public BaseballGame(String computerNumbers) {
+        this.computerNumbers = computerNumbers;
+    }
+
+    public GameResult compareNumbers(String userNumbers) {
+        currentGameResult = new GameResult();
+
+        for (int i = 0; i < userNumbers.length(); i++) {
+            compareNumber(userNumbers.charAt(i), computerNumbers.charAt(i));
+        }
+
+        return currentGameResult;
+    }
+
+    public void compareNumber(char userNum, char computerNum) {
+        if (!computerNumbers.contains(String.valueOf(userNum))) {
+            return;
+        }
+
+        if (userNum == computerNum) {
+            currentGameResult.increaseStrikeCount();
+            return;
+        }
+        currentGameResult.increaseBallCount();
+    }
+
+    public boolean isEnd() {
+        return currentGameResult.getStrikeCount() == 3;
+    }
+
+}
