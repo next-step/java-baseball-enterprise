4121fc238cb0173b9a950daf01c08e16db9943c1
엄태호 <qqsscc7342@gmail.com>
2022-12-20 11:35:45 +0900

    test: SetTest 요구사항 2

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -28,5 +28,10 @@ public class SetTest {
     void size() {
         assertThat(numbers.size()).isEqualTo(3);
     }
-    
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
 }
