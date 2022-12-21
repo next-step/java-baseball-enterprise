150ba7a03ae57df9b9e3942553122beeaa3dc5ff
gh1224 <rlarkgus0@naver.com>
2022-12-22 00:01:31 +0900

    test: 랜덤 숫자 3개 생성 테스트
    1. 1~9의 숫자
    2. 서로 다른 숫자
    
    private 멤버 변수값을 reflection으로 가져와 테스트

--- src/test/java/NumSetTest.java
+++ src/test/java/NumSetTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
+
 import static org.assertj.core.api.Assertions.*;
 
 class NumSetTest {
@@ -7,5 +9,24 @@ class NumSetTest {
     void convertToList() {
         assertThat(NumSet.convertToList(123)).isEqualTo(new int[]{1, 2, 3});
         assertThat(NumSet.convertToList(719)).isEqualTo(new int[]{7, 1, 9});
+    }
+
+    @Test
+    void generateRandom3Number() throws NoSuchFieldException, IllegalAccessException {
+        Field numArrField = NumSet.class.getDeclaredField("numArr");
+        numArrField.setAccessible(true);
+
+        for (int i = 0; i < 200; i++) {
+            NumSet numSet = new NumSet();
+            int[] numArr = (int[]) numArrField.get(numSet);
+
+            assertThat(numArr[0]).isBetween(1, 9);
+            assertThat(numArr[1]).isBetween(1, 9);
+            assertThat(numArr[2]).isBetween(1, 9);
+
+            assertThat(numArr[0]).isNotEqualTo(numArr[1]);
+            assertThat(numArr[1]).isNotEqualTo(numArr[2]);
+            assertThat(numArr[2]).isNotEqualTo(numArr[0]);
+        }
     }
 }
