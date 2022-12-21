635ad9f17251cbbdee28eee07694b9a52ffa18ea
park0503 <hooo0503@gmail.com>
2022-12-22 04:40:16 +0900

    test: InputValidator 테스트 구현
    - 올바른 format의 input 테스트
    - 올바르지 않은 format의 input 테스트

--- src/main/java/study/baseball/engine/InputValidator.java
+++ src/main/java/study/baseball/engine/InputValidator.java
@@ -1,8 +1,5 @@
 package study.baseball.engine;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import java.util.regex.Pattern;
 
 public class InputValidator {
@@ -13,9 +10,10 @@ public class InputValidator {
         this.regex = regex;
     }
 
-    public void checkValidFormat(String input) {
+    public boolean checkValidFormat(String input) {
         if(!regex.matcher(input).matches()) {
             throw new RuntimeException("입력의 형식이 잘못되었습니다.");
         }
+        return true;
     }
 }
