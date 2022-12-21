60ff7333e328263107c6814f7b5dc94b22165d57
ekzm8523 <mjw8523@gmail.com>
2022-12-21 17:10:30 +0900

    test: 유저가 서로 다른 3자리의 수를 입력한다.

---
+++ src/test/java/baseball/view/BaseballViewTest.java
@@ -0,0 +1,56 @@
+package baseball.view;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BaseballViewTest {
+
+    BaseballView baseballView;
+
+    @BeforeEach
+    public void beforeEach() {
+        baseballView = new BaseballView();
+    }
+
+    @DisplayName("입력이 3글자가 아니면 Exception이 발생합니다.")
+    @Test
+    void isValidLength() {
+        // given
+        String fourLengthInput = "1234";
+
+        // when & then
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> baseballView.validatePlayerInput(fourLengthInput)
+        );
+    }
+
+    @DisplayName("숫자가 아닌 다른 값이 input으로 들어가면 Exception이 발생합니다.")
+    @Test
+    void isOnlyNumber() {
+        // given
+        String notOnlyNumberInput = "12#";
+
+        // when & then
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> baseballView.validatePlayerInput(notOnlyNumberInput)
+        );
+    }
+
+    @DisplayName("중복된 숫자를 입력하면 Exception이 발생합니다.")
+    @Test
+    void isNotDuplicated() {
+        // given
+        String duplicatedNumberInput = "112";
+
+        // when & then
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> baseballView.validatePlayerInput(duplicatedNumberInput)
+        );
+    }
+}
