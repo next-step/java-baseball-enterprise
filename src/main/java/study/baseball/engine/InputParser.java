d48a06f6d14c6bfe365419b8df333c43ae58debe
park0503 <hooo0503@gmail.com>
2022-12-22 04:49:57 +0900

    test: InputParser 테스트 구현
    - InputParser 테스트 구현
    - 입력값 중복처리 제거

--- src/main/java/study/baseball/engine/InputParser.java
+++ src/main/java/study/baseball/engine/InputParser.java
@@ -4,14 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InputParser {
-    public List<Integer> parseStringToIntegerList(String input) throws NumberFormatException {
+    public List<Integer> parseStringToIntegerList(String input) {
         String[] inputArray = input.split("");
         List<Integer> result = new ArrayList<>();
         for (String number:inputArray) {
             result.add(Integer.parseInt(number));
-        }
-        if (input.length() != result.size()) {
-            throw new RuntimeException("중복된 숫자가 존재합니다.");
         }
         return result;
     }
