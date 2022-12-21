f71ad914e3767e829e33b51e4e52c34b6028d5ee
Minsu Kim <ravor8878@gmail.com>
2022-12-21 13:21:30 +0900

    feat : add manager.ProcessManager and main.Main
    manager.ProcessManager - just call execute() for game start.
    main.Main - make executable program

--- src/main/java/num3baseball/main/Main.java
+++ src/main/java/num3baseball/main/Main.java
@@ -1,2 +1,12 @@
-package num3baseball.main;public class main {
+package num3baseball.main;
+
+import num3baseball.manager.ProcessManager;
+import num3baseball.ui.GameUserInterfaceKorean;
+import num3baseball.ui.GameUserInterfaceTestStub;
+
+public class Main {
+    public static void main(String[] args){
+        ProcessManager processManager = new ProcessManager(new GameUserInterfaceKorean());
+        processManager.execute();
+    }
 }
