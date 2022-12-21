a630a56ff0db95886dffbb21d707a94ba71fb905
park0503 <hooo0503@gmail.com>
2022-12-22 02:58:47 +0900

    feat: 랜덤 숫자 생성 메소드 구현
    - NumberGenerator 클래스 내 랜덤 숫자 생성 메소드 구현
    - Numbers model 생성

---
+++ src/main/java/study/baseball/engine/model/Numbers.java
@@ -0,0 +1,11 @@
+package study.baseball.engine.model;
+
+import java.util.Set;
+
+public class Numbers {
+
+    private final Set<Integer> numbers;
+    public Numbers(Set<Integer> numbers) {
+        this.numbers = numbers;
+    }
+}
