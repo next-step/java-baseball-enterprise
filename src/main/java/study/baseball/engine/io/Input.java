06dc8e976d7058375f263d029dad49ceb500bcdd
park0503 <hooo0503@gmail.com>
2022-12-22 03:18:40 +0900

    feat: 입력, 출력 담당 콘솔 구현
    - Input 인터페이스 구현
    - Output 인터페이스 구현
    - Console 구현체 구현

---
+++ src/main/java/study/baseball/engine/io/Input.java
@@ -0,0 +1,5 @@
+package study.baseball.engine.io;
+
+public interface Input {
+    String input(String s);
+}
---
+++ src/main/java/study/baseball/engine/io/Output.java
@@ -0,0 +1,7 @@
+package study.baseball.engine.io;
+
+public interface Output {
+    void printError(String s);
+
+    void printOutput(String s);
+}
