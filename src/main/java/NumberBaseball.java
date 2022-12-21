94a1bacaf2d3b4c4b9dffbf03eb53adc8b4ddf15
Seongsu Park <86999506+HolywaterPark@users.noreply.github.com>
2022-12-21 18:30:21 +0900

    refac(): 출력과 continue 여부 로직 분리

--- src/main/java/NumberBaseball.java
+++ src/main/java/NumberBaseball.java
@@ -79,16 +79,18 @@ public class NumberBaseball {
         System.out.print("\n");
     }
 
+    public boolean continue_or_end(int input){
+        if (input == CONTINUE) return true;
+        if (input == END) return false;
+        return false;
+    }
+
     public boolean print_end_message(){
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        while(true) {
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
-            Scanner scanner = new Scanner(System.in);
-            int input = Integer.parseInt(scanner.nextLine());
-            if (input == CONTINUE) return true;
-            if (input == END) return false;
-            System.out.println("잘못 입력하셨습니다");
-        }
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+        Scanner scanner = new Scanner(System.in);
+        int input = Integer.parseInt(scanner.nextLine());
+        return continue_or_end(input);
     }
 
     public boolean do_game() {
