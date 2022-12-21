07209f441353ee29644ef515ee7816aea1fc91f9
person003333 <person00333@gmail.com>
2022-12-21 15:32:39 +0900

    feat: Application 작성
    프로그램을 실행하는 main문 작성

---
+++ src/main/java/Application.java
@@ -0,0 +1,12 @@
+import controller.BaseBallController;
+
+import java.io.IOException;
+
+public class Application {
+
+    public static void main(String[] args) throws IOException {
+        BaseBallController baseBallController = new BaseBallController();
+        baseBallController.BaseBallGame();
+
+    }
+}
