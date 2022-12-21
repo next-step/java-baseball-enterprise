81002ccb32dbd455e1b9ea843db2a34574a1824d
siryeong <siryeongchoi@gmail.com>
2022-12-21 16:56:24 +0900

    feat(baseball): 메인 로직 추가
    메인 로직 추가
    - 게임 완전 종료 조건 체크

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,19 @@
 import baseball.*;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		boolean continueGame;
+
         // app start
 		Baseball game = new BaseballImpl();
-		game.start();
+		do{
+			game.start();
+			System.out.print("계속 하시겠습니까? (y/n) : ");
+			String command = sc.nextLine();
+			continueGame = command.charAt(0) == 'y' || command.charAt(0) == 'Y';
+		} while (continueGame);
     }
 }
