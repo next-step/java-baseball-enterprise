7a0494a2dac6bd1e0ad673a033ba09c8158f7b9e
siryeong <siryeongchoi@gmail.com>
2022-12-21 16:25:04 +0900

    test(baseball): 매칭 테스트코드 작성
    매칭 테스트코드 작성
    - 유저 입력 valid check
    - 두 수의 비교 결과가 잘 출력 되는지 확인

--- src/test/java/baseball/BaseballTest.java
+++ src/test/java/baseball/BaseballTest.java
@@ -6,13 +6,47 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 public class BaseballTest {
+	Baseball game = new BaseballImpl();
 
 	@Test
 	@DisplayName("컴퓨터의 랜덤 숫자 생성 테스트")
 	void computerGenerateNumbers() {
-		Baseball game = new BaseballImpl();
-		int[] numbers = game.generateNumber();
-		assertThat(numbers.length).isEqualTo(3);
-		assertThat(game.checkValidOfNumber(numbers)).isTrue();
+		int[] number = game.generateNumber();
+		assertThat(number.length).isEqualTo(3);
+		assertThat(game.checkValidOfNumber(number)).isTrue();
+	}
+
+	@Test
+	@DisplayName("유저 입력 테스트")
+	void playerInputValidTest() {
+		int[] validNumber = {1,2,3};
+		assertThat(game.checkValidOfNumber(validNumber)).isTrue();
+
+		int[] outOfBound1 = {0,1,2};
+		assertThat(game.checkValidOfNumber(outOfBound1)).isFalse();
+		int[] outOfBound2 = {1,2,10};
+		assertThat(game.checkValidOfNumber(outOfBound2)).isFalse();
+
+		int[] invalidLength1 = {1,2};
+		assertThat(game.checkValidOfNumber(invalidLength1)).isFalse();
+		int[] invalidLength2 = {1,2,3,4};
+		assertThat(game.checkValidOfNumber(invalidLength2)).isFalse();
+	}
+
+	@Test
+	@DisplayName("숫자 매칭 테스트")
+	void matchResultTest() {
+		int[] computerNumber = {1,2,3};
+		int[] correctNumber = {1,2,3};
+		assertThat(game.getMatchResult(computerNumber, correctNumber)).containsExactly(3, 0);
+
+		int[] threeBall = {2,3,1};
+		assertThat(game.getMatchResult(computerNumber, threeBall)).containsExactly(0, 3);
+
+		int[] oneStrikeOneBall = {1, 3, 4};
+		assertThat(game.getMatchResult(computerNumber, oneStrikeOneBall)).containsExactly(1, 1);
+
+		int[] nothing = {4,5,6};
+		assertThat(game.getMatchResult(computerNumber, nothing)).containsExactly(0, 0);
 	}
 }
