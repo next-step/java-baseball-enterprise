c8ef5a9085d16dae517261687e153ed8036e6bd1
kane.woz <kane.woz@kakaocorp.com>
2022-12-21 12:55:40 +0900

    feat($src) : 1차 기능 구현 Game, GameResult

---
+++ src/main/java/game/Game.java
@@ -0,0 +1,68 @@
+package game;
+
+public class Game {
+    public final int[] actualValues;
+
+    private Game(int[] actualValues) {
+        if (actualValues == null) {
+            throw new RuntimeException("actualValues is null");
+        }
+        if (actualValues.length != 3) {
+            throw new RuntimeException(String.format("actualValues length expect 3, but got %d", actualValues.length));
+        }
+        this.actualValues = actualValues;
+    }
+
+    public static int[] randomPick() {
+        return new int[]{
+                (int) Math.min((Math.random() * 9. + 1), 9.),
+                (int) Math.min((Math.random() * 9. + 1), 9.),
+                (int) Math.min((Math.random() * 9. + 1), 9.),
+        };
+    }
+    public static Game randomGame() {
+        return new Game(Game.randomPick());
+    }
+
+    // 검사 없는 ball 개수 확인 함수
+    private int countBallUnsafe(int[] expectValues) {
+        assert expectValues != null;
+        assert expectValues.length == 3;
+        // 개수 확인 집계용도
+        int count = 0;
+        // 길이 동일여부 확인은 위에서 확인됨
+        for (int i = 0; i < 3; i++) {
+            for (int j = i + 1; j < 3; j++) {
+                if (this.actualValues[i] == expectValues[j]) {
+                    count += 1;
+                }
+            }
+        }
+        return count;
+    }
+    
+    // 검사 없는 ball 개수 확인 함수
+    private int countStrikeUnsafe(int[] expectValues) {
+        assert expectValues != null;
+        assert expectValues.length == 3;
+        // 개수 확인 집계용도
+        int count = 0;
+        // 길이 동일여부 확인은 위에서 확인됨
+        for (int i = 0; i < 3; i++) {
+            if (this.actualValues[i] == expectValues[i]) {
+                count += 1;
+            }
+        }
+        return count;
+    }
+
+    public GameResult calculateGameResults(int[] expectValues) {
+        if (expectValues == null) {
+            throw new RuntimeException("expectValue is null");
+        }
+        if (expectValues.length != 3) {
+            throw new RuntimeException(String.format("expectValue length expect 3, but got %d", expectValues.length));
+        }
+        return new GameResult(this.countBallUnsafe(expectValues), countStrikeUnsafe(expectValues));
+    }
+}
---
+++ src/main/java/game/GameResult.java
@@ -0,0 +1,33 @@
+package game;
+
+public class GameResult {
+    public int ball;
+    public int strike;
+
+    GameResult(int ball, int strike) {
+        if (ball + strike > 3) {
+            throw new RuntimeException(String.format("unexpected ball(=%d) and strike(=%d)", ball, strike));
+        }
+        this.ball = ball;
+        this.strike = strike;
+    }
+
+    public boolean isNothing() {
+        return this.strike == 0 && this.ball != 0;
+    }
+
+    public boolean isAllStrike() {
+        return this.strike == 3 && this.ball == 0;
+    }
+
+    @Override
+    public String toString() {
+        if (this.isNothing()) {
+            return "nothing";
+        }
+        if (this.isAllStrike()) {
+            return "all strike";
+        }
+        return String.format("%d strike, %d ball", this.strike, this.ball);
+    }
+}
