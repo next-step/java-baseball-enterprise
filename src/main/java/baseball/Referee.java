8272343de437ecd5c07fa1726234c9e32c30d661
kakao <coolofficials@gmail.com>
2022-12-22 18:08:51 +0900

    feat: Referee

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -1,8 +1,45 @@
 package baseball;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Referee {
     public int[] judge(String target, String input) {
-        int[] result = {2, 2};
-        return result;
+        return new int[] {countStrike(convert(target), convert(input)), countBall(convert(target), convert(input))};
+    }
+
+    private List<Integer> convert(String string) {
+        List<Integer> career = new ArrayList<>();
+        for(String number: string.split("")) {
+            career.add(Integer.parseInt(number));
+        }
+        return career;
+    }
+
+    private int countBall(List<Integer> target, List<Integer> input) {
+        int ball = 0;
+        if (target.get(0) == input.get(1) || target.get(0) == input.get(2)) {
+            ball++;
+        }
+        if (target.get(1) == input.get(0) || target.get(1) == input.get(2)) {
+            ball++;
+        }
+        if (target.get(2) == input.get(0) || target.get(2) == input.get(1)) {
+            ball++;
+        }
+        return ball;
+    }
+
+    private int countStrike(List<Integer> target, List<Integer> input) {
+        int strike = 0;
+        if (target.get(0) == input.get(0)) {
+            strike ++;
+        }
+        if (target.get(1) == input.get(1)) {
+            strike ++;
+        }
+        if (target.get(2) == input.get(2)) {
+            strike ++;
+        }
+        return strike;
     }
 }
