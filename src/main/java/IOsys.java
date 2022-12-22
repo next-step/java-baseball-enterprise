6dd5b6149538d38a7ebda52b60b9d9195eba1a2c
kakao <kakao@kakaoui-MacBookPro.local>
2022-12-22 18:31:13 +0900

    [feat] add user input validator - check duplicated number and out of range number - add code in IOsys userIn() func

--- src/main/java/IOsys.java
+++ src/main/java/IOsys.java
@@ -9,9 +9,18 @@ public class IOsys {
     public int userIn(){
         System.out.print("숫자를 입력해주세요: ");
         int userinput = sc.nextInt();
-
+        while(userinput < 100 || 1000 <= userinput || check_dup(userinput)){
+            System.out.println("불가능한 입력입니다.");
+            System.out.print("숫자를 입력해주세요: ");
+            userinput = sc.nextInt();
+        }
         return userinput;
     }
+
+    public boolean check_dup(int userinput){
+        return userinput/100 == userinput%10 || userinput/100 == userinput%100/10 || userinput%10 == userinput%100/10;
+    }
+
     public void print_result(Checker C){
         if(C.getStrike()!= 0) System.out.printf("%d스트라이크 ", C.getStrike());
         if(C.getBall() != 0) System.out.printf("%d볼 ", C.getBall());
