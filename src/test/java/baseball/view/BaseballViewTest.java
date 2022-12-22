c4e3fa4ea7349ba2a2f8a943fa8bd127a31e44e7
ekzm8523 <mjw8523@gmail.com>
2022-12-22 22:12:14 +0900

    test: 결과를 종합한다.

--- src/test/java/baseball/view/BaseballViewTest.java
+++ src/test/java/baseball/view/BaseballViewTest.java
@@ -1,9 +1,10 @@
 package baseball.view;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import static org.assertj.core.api.Assertions.assertThat;
 import baseball.controller.BaseballController;
 import baseball.domain.BaseballNumber;
+import baseball.dto.BaseballResultDto;
 import baseball.service.BaseballService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -58,5 +59,44 @@ class BaseballViewTest {
                 IllegalArgumentException.class,
                 () -> baseballView.validatePlayerInput(duplicatedNumberInput)
         );
+    }
+
+    @DisplayName("하나도 맞지 않으면 '낫싱'이 반환됩니다.")
+    @Test
+    void getBaseballResultString() {
+        // given
+        BaseballResultDto baseballResultDto = new BaseballResultDto(0, 0);
+
+        // when
+        String resultString = baseballView.getBaseballResultString(baseballResultDto);
+
+        // then
+        assertThat(resultString).isEqualTo("낫싱");
+    }
+
+    @DisplayName("스트라이크가 0이 아니면 스트라이크에 대한 정보가 포함됩니다.")
+    @Test
+    void getStrikeString() {
+        // given
+        BaseballResultDto baseballResultDto = new BaseballResultDto(1, 0);
+
+        // when
+        String resultString = baseballView.getBaseballResultString(baseballResultDto);
+
+        // then
+        assertThat(resultString).contains("스트라이크");
+    }
+
+    @DisplayName("볼이 0이 아니면 볼에 대한 정보가 포함됩니다.")
+    @Test
+    void getBallString() {
+        // given
+        BaseballResultDto baseballResultDto = new BaseballResultDto(0, 1);
+
+        // when
+        String resultString = baseballView.getBaseballResultString(baseballResultDto);
+
+        // then
+        assertThat(resultString).contains("볼");
     }
 }
