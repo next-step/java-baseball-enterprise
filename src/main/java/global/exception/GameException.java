caec9bdb5b94c0ba0d1fdaf1971554f02091cada
BaeRoNuI <xogh20321@gmail.com>
2022-12-22 10:08:42 +0900

    feat: add GameException

---
+++ src/main/java/global/exception/GameException.java
@@ -0,0 +1,20 @@
+package global.exception;
+
+public class GameException extends RuntimeException {
+
+    public GameException() {
+
+    }
+
+    public GameException(String message) {
+        super(message);
+    }
+
+    public GameException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public GameException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
