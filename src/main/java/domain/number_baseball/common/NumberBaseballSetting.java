dc5270b49ef7a634f517dbd2628b5c72f75e66b1
BaeRoNuI <xogh20321@gmail.com>
2022-12-22 10:12:15 +0900

    feat: add BaseBallSetting

---
+++ src/main/java/domain/number_baseball/common/NumberBaseballSetting.java
@@ -0,0 +1,10 @@
+package domain.number_baseball.common;
+
+public class NumberBaseballSetting {
+    private NumberBaseballSetting() {}
+
+    public static final int MAX_BALL_NUMBER = 3;
+    public static final String TRUE_INPUT = "1";
+    public static final String FALSE_INPUT = "2";
+    public static final Integer[] NUMBER_PRESET = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+}
