e6a0350425605d9ddab55f782bc546ce468751ad
kakao <kakao@kakaoui-MacBookPro.local>
2022-12-22 09:47:29 +0900

    [feat] add retry feature - add game class - handle main code in game class - 2 loop is divided by 2 functions

--- server_mission/src/main/java/org/example/IOsys.java
+++ server_mission/src/main/java/org/example/IOsys.java
@@ -2,10 +2,12 @@ package org.example;
 import java.util.Scanner;
 
 public class IOsys {
+    private Scanner sc;
 
+    public IOsys(){
+        sc = new Scanner(System.in);
+    }
     public int userIn(){
-        Scanner sc = new Scanner(System.in);
-
         System.out.print("숫자를 입력해주세요: ");
         int userinput = sc.nextInt();
 
@@ -15,5 +17,13 @@ public class IOsys {
         if(C.getStrike()!= 0) System.out.printf("%d스트라이크 ", C.getStrike());
         if(C.getBall() != 0) System.out.printf("%d볼 ", C.getBall());
         if(C.getBall() + C.getStrike() == 0) System.out.printf("낫싱");
+        System.out.println();
+    }
+    public int nextgame(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        int ContinueFlag = sc.nextInt();
+
+        return ContinueFlag;
     }
 }
--- server_mission/src/main/java/org/example/Main.java
+++ server_mission/src/main/java/org/example/Main.java
@@ -2,23 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
-        while(true){
-
-            Opponent OP = new Opponent();
-            OP.generate_random_int();
-            System.out.println(OP.getRandom_Int());
-
-            IOsys IO;
-            IO = new IOsys();
-            int userinput = IO.userIn();
-
-            Checker chk = new Checker(OP);
-            chk.compare(userinput, OP.getRandom_Int());
-            IO.print_result(chk);
-
-            //int[] info_arr = {0,0,0};// strike, ball,
-
-        }
+        game g = new game();
+        g.game_start();
     }
 }
