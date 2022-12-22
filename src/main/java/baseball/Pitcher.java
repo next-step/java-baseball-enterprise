2b8ea407b0875d55f9a986912e9f89c796ef8a1a
kakao <coolofficials@gmail.com>
2022-12-22 17:22:20 +0900

    feat: generateNumber in Pitcher

--- src/main/java/baseball/Pitcher.java
+++ src/main/java/baseball/Pitcher.java
@@ -1,7 +1,19 @@
 package baseball;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Pitcher {
     public String generateNumber() {
-        return "123";
+        Random random = new Random();
+        ArrayList<Integer> digits = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+        StringBuilder number = new StringBuilder();
+        for (int i = 0; i < 3; i++) {
+            int randomIndex = random.nextInt(digits.size());
+            number.append(digits.get(randomIndex));
+            digits.remove(randomIndex);
+        }
+
+        return number.toString();
     }
 }
