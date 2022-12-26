59be189abf6ed27ecb72721f297af7acf682ad99
park0503 <hooo0503@gmail.com>
2022-12-26 11:17:36 +0900

    feat: 입력값 중복검사 로직 추가
    - InputParser의 parsing 메소드에 중복된 숫자 검사 로직 추가

--- src/main/java/study/baseball/engine/InputParser.java
+++ src/main/java/study/baseball/engine/InputParser.java
@@ -1,15 +1,20 @@
 package study.baseball.engine;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 public class InputParser {
     public List<Integer> parseStringToIntegerList(String input) {
         String[] inputArray = input.split("");
-        List<Integer> result = new ArrayList<>();
+        Set<Integer> result = new LinkedHashSet<>();
         for (String number : inputArray) {
             result.add(Integer.parseInt(number));
         }
-        return result;
+        if (result.size() < 3) {
+            throw new RuntimeException("중복된 숫자가 존재합니다.");
+        }
+        return new ArrayList<>(result);
     }
 }
--- src/test/java/study/baseball/engine/InputParserTest.java
+++ src/test/java/study/baseball/engine/InputParserTest.java
@@ -1,6 +1,7 @@
 package study.baseball.engine;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -14,5 +15,13 @@ class InputParserTest {
     public void testParseValidInput(String input) {
         List<Integer> numbers = parser.parseStringToIntegerList(input);
         assertThat(numbers).hasSize(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"777", "112", "911", "707"})
+    public void testParseDuplicatedInput(String input) {
+        assertThatThrownBy(() -> {
+            List<Integer> numbers = parser.parseStringToIntegerList(input);
+        }).isInstanceOf(RuntimeException.class).hasMessage("중복된 숫자가 존재합니다.");
     }
 }
