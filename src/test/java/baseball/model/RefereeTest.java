a05860e75c4c88af110ac2bfbc982ad6908fc64a
wilgur513 <wilgur513@naver.com>
2023-02-27 14:03:40 +0900

    docs: README.md 수정

--- src/test/java/baseball/model/RefereeTest.java
+++ src/test/java/baseball/model/RefereeTest.java
@@ -9,37 +9,54 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class RefereeTest {
 
-    @DisplayName("낫싱으로 판결한다.")
+    @DisplayName("결과를 판결한다.")
     @Test
     void judgeNothing() {
-        Numbers rightNumbers = Numbers.from("123");
-        Numbers inputNumbers = Numbers.from("456");
-        Referee referee = new Referee(rightNumbers);
+        Referee referee = new Referee(Numbers.from("123"));
 
-        Judgements actual = referee.judge(inputNumbers);
-
+        Judgements actual = referee.judge(Numbers.from("456"));
         assertThat(actual).hasStrike(0).hasBall(0);
+
+        actual = referee.judge(Numbers.from("145"));
+        assertThat(actual).hasStrike(1).hasBall(0);
     }
 
     @DisplayName("1스트라이크로 판결한다.")
     @ParameterizedTest
     @CsvSource({"123, 145", "123, 425", "123, 453"})
-    void judgeOneStrike(String rightNumbersText, String inputNumbersText) {
-        Numbers rightNumbers = Numbers.from(rightNumbersText);
-        Numbers inputNumbers = Numbers.from(inputNumbersText);
-        Referee referee = new Referee(rightNumbers);
+    void judgeOneStrike(String rightNumbers, String inputNumbers) {
+        Referee referee = new Referee(Numbers.from(rightNumbers));
 
-        Judgements actual = referee.judge(inputNumbers);
+        Judgements actual = referee.judge(Numbers.from(inputNumbers));
+
+        assertThat(actual).hasStrike(1).hasBall(0);
+    }
+
+    @DisplayName("두번째 자리의 숫자만 같은 경우 1스트라이크로 판결한다.")
+    @Test
+    void judgeOneStrikeSecondDigit() {
+        Referee referee = new Referee(Numbers.from("123"));
+
+        Judgements actual = referee.judge(Numbers.from("425"));
+
+        assertThat(actual).hasStrike(1).hasBall(0);
+    }
+
+    @DisplayName("세번째 자리의 숫자만 같은 경우 1스트라이크로 판결한다.")
+    @Test
+    void judgeOneStrikeThirdDigit() {
+        Referee referee = new Referee(Numbers.from("123"));
+
+        Judgements actual = referee.judge(Numbers.from("453"));
 
         assertThat(actual).hasStrike(1).hasBall(0);
     }
 
     @DisplayName("2스트라이크로 판결한다.")
-    @ParameterizedTest
-    @CsvSource({"123, 124", "456, 156", "789, 759"})
-    void judgeTwoStrike(String rightNumbersText, String inputNumbersText) {
-        Numbers rightNumbers = Numbers.from(rightNumbersText);
-        Numbers inputNumbers = Numbers.from(inputNumbersText);
+    @Test
+    void judgeTwoStrike() {
+        Numbers rightNumbers = Numbers.from("123");
+        Numbers inputNumbers = Numbers.from("124");
         Referee referee = new Referee(rightNumbers);
 
         Judgements actual = referee.judge(inputNumbers);
@@ -48,11 +65,10 @@ public class RefereeTest {
     }
 
     @DisplayName("1볼로 판결한다.")
-    @ParameterizedTest
-    @CsvSource({"123, 415", "123, 451", "123, 245", "123, 452", "123, 345", "123, 435"})
-    void judgeOneBall(String rightNumbersText, String inputNumbersText) {
-        Numbers rightNumbers = Numbers.from(rightNumbersText);
-        Numbers inputNumbers = Numbers.from(inputNumbersText);
+    @Test
+    void judgeOneBall() {
+        Numbers rightNumbers = Numbers.from("123");
+        Numbers inputNumbers = Numbers.from("415");
         Referee referee = new Referee(rightNumbers);
 
         Judgements actual = referee.judge(inputNumbers);
@@ -61,11 +77,10 @@ public class RefereeTest {
     }
 
     @DisplayName("2볼로 판결한다.")
-    @ParameterizedTest
-    @CsvSource({"123, 215", "123, 251"})
-    void judgeTwoBall(String rightNumbersText, String inputNumbersText) {
-        Numbers rightNumbers = Numbers.from(rightNumbersText);
-        Numbers inputNumbers = Numbers.from(inputNumbersText);
+    @Test
+    void judgeTwoBall() {
+        Numbers rightNumbers = Numbers.from("123");
+        Numbers inputNumbers = Numbers.from("215");
         Referee referee = new Referee(rightNumbers);
 
         Judgements actual = referee.judge(inputNumbers);
